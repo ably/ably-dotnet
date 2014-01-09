0494b94fa9ad0fc45a417facb4dcc4815d1b3702
Martin Georgiev <marto83@gmail.com>
2014-01-09 23:05:39 +0000

    Moved DataRequestParseFacts to separate file

--- src/Ably.Tests/DataRequestQueryTests.cs
+++ src/Ably.Tests/DataRequestQueryTests.cs
@@ -3,53 +3,6 @@ using Xunit;
 
 namespace Ably.Tests
 {
-    public class DataRequestQueryParseFacts
-    {
-        private DataRequestQuery _query;
-        public const string QueryString = "?start=1380794880000&end=1380794881058&limit=100&by=minute&direction=forwards&format=json&first_start=1380794880000";
-
-        public DataRequestQueryParseFacts()
-        {
-            _query = DataRequestQuery.Parse(QueryString);
-        }
-
-        [Fact]
-        public void Parse_WithQueryString_SetsCorrectStartAndEndDates()
-        {
-            //Arrange
-            var startDate = long.Parse("1380794880000").FromUnixTimeInMilliseconds();
-            var endDate = long.Parse("1380794881058").FromUnixTimeInMilliseconds();
-            
-            //Assert
-            Assert.Equal(startDate, _query.Start);
-            Assert.Equal(endDate, _query.End);
-        }
-
-        [Fact]
-        public void Parse_WithQueryString_SetsCorrectLimit()
-        {
-            Assert.Equal(100, _query.Limit);
-        }
-
-        [Fact]
-        public void Parse_SetsCorrectDirection()
-        {
-            //Assert
-            Assert.Equal(QueryDirection.Forwards, _query.Direction);
-        }
-
-        [Fact]
-        public void Parse_HasTwoExtraParameters()
-        {
-            //Assert
-            Assert.Equal(3, _query.ExtraParameters.Count);
-            Assert.True(_query.ExtraParameters.ContainsKey("by"));
-            Assert.True(_query.ExtraParameters.ContainsKey("first_start"));
-            Assert.True(_query.ExtraParameters.ContainsKey("format"));
-        }
-        
-    }
-
     public class DataRequestQueryTests
     {
         public static NameValueCollection GetSampleHistoryRequestHeaders()
