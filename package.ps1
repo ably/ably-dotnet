f33b734eaac6a66c5696d500e6f6276049745468
Yavor <yavor87@gmail.com>
2015-09-07 11:22:23 +0300

    adding package script

---
+++ package.ps1
@@ -0,0 +1,3 @@
+C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\msbuild.exe /property:Configuration=Release .\\src\\Ably.sln
+C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\msbuild.exe /property:Configuration="Release 4.0" .\\src\\Ably.sln
+.\\tools\\NuGet.exe pack .\\nuget\\ably.io.nuspec
