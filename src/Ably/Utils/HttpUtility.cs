74c0321c774af3f428d28246dacdd705f2543fff
Yavor Ivanov <yavor@newventuresoftware.com>
2015-12-02 19:04:56 +0200

    Adding xamarin project for android

--- src/Ably/Utils/HttpUtility.cs
+++ src/Ably/Utils/HttpUtility.cs
@@ -2,15 +2,15 @@
 {
     internal class HttpUtility
     {
-#if NET
-        public static string UrlEncode(string str)
-        {
-            return System.Web.HttpUtility.UrlEncode(str);
-        }
-#elif SILVERLIGHT
+#if SILVERLIGHT
         public static string UrlEncode(string str)
         {
             return System.Windows.Browser.HttpUtility.UrlEncode(str);
+        }
+#else
+        public static string UrlEncode(string str)
+        {
+            return System.Web.HttpUtility.UrlEncode(str);
         }
 #endif
     }
