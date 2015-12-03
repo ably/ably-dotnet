a229f8e6b76d5c84f7fdd126b67ec4d62baf907d
Yavor <yavor87@gmail.com>
2015-12-03 16:01:27 +0200

    Fix for query string

--- src/Ably/Utils/QueryStringUtility.cs
+++ src/Ably/Utils/QueryStringUtility.cs
@@ -1,18 +1,28 @@
-﻿using RestSharp.Extensions.MonoHttp;
-using System.Collections.Specialized;
+﻿using System.Linq;
+#if SILVERLIGHT
+using SW = System.Windows.Browser;
+using SCS = Ably.Utils;
+#else
+using SW = RestSharp.Extensions.MonoHttp;
+using SCS = System.Collections.Specialized;
+#endif
 
 namespace Ably.Utils
 {
     internal static class QueryStringUtility
     {
-        public static NameValueCollection CreateQueryCollection()
+        public static SCS.NameValueCollection CreateQueryCollection()
         {
-            return HttpUtility.ParseQueryString("");
+            return new SCS.NameValueCollection();
         }
 
-        public static string ToQueryString(NameValueCollection collection)
+        public static string ToQueryString(this SCS.NameValueCollection collection)
         {
-            return collection.ToString();
+            var array = (from key in collection.AllKeys
+                         from value in collection.GetValues(key)
+                         select string.Format("{0}={1}", SW.HttpUtility.UrlEncode(key), SW.HttpUtility.UrlEncode(value)))
+                         .ToArray();
+            return string.Join("&", array);
         }
     }
 }
