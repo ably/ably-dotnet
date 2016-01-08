078a48938b8f1fa1bf9bb80731bcad2aa02115d7
Konstantin <soonts@live.com>
2016-01-09 00:00:26 +0100

    Portable (incomplete)

--- src/Ably.Platforms/Ably.net45/PlatformImpl.cs
+++ src/Ably.Platforms/Ably.net45/PlatformImpl.cs
@@ -1,14 +1,34 @@
 ﻿using Ably.Platform;
+using Ably.Transport;
+using System.Configuration;
 
 namespace Ably
 {
     public class PlatformImpl : IPlatform
     {
+        string IPlatform.getConnectionString()
+        {
+            var connString = ConfigurationManager.ConnectionStrings["Ably"];
+            if( connString == null )
+            {
+                return string.Empty;
+            }
+            return connString.ConnectionString;
+        }
+
         ICrypto IPlatform.crypto
         {
             get
             {
                 return new Cryptography.CryptoImpl();
+            }
+        }
+
+        ITransportFactory IPlatform.webSockets
+        {
+            get
+            {
+                return new WebSocketTransport.WebSocketTransportFactory();
             }
         }
     }
