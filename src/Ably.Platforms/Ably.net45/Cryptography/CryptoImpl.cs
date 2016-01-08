eba54155020a8cc193f4d1170f86e77004b90adf
Konstantin <soonts@live.com>
2016-01-08 23:45:14 +0100

    Portable (incomplete)

---
+++ src/Ably.Platforms/Ably.net45/Cryptography/CryptoImpl.cs
@@ -0,0 +1,32 @@
+﻿using Ably.Platform;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using Ably.Rest;
+
+namespace Ably.Cryptography
+{
+    internal class CryptoImpl : ICrypto
+    {
+        string ICrypto.ComputeHMacSha256( string text, string key )
+        {
+            byte[] bytes = text.GetBytes();
+            byte[] keyBytes = key.GetBytes();
+            using( var hmac = new HMACSHA256( keyBytes ) )
+            {
+                hmac.ComputeHash( bytes );
+                return Convert.ToBase64String( hmac.Hash );
+            }
+        }
+
+        IChannelCipher ICrypto.GetCipher( ChannelOptions opts )
+        {
+            throw new NotImplementedException();
+        }
+
+        CipherParams ICrypto.GetDefaultParams()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
