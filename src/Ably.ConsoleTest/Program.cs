f273a635903a634921d85a7d55745515452943b3
Konstantin <soonts@live.com>
2016-01-08 16:34:42 +0100

    Created console app that runs acceptance tests.

---
+++ src/Ably.ConsoleTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using Ably.AcceptanceTests;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Ably.ConsoleTest
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Assembly ass = typeof(LoggerTests).Assembly;
+            string path = ass.Location;
+
+            List<string> options = new List<string>();
+            options.Add( path );
+            options.Add( "/domain:None" );
+            options.Add( "/nothread" );
+
+            NUnit.ConsoleRunner.Runner.Main( options.ToArray() );
+        }
+    }
+}
