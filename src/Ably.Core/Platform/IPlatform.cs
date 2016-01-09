79b11dcc51434c4987e8e20f0dae1ac99b9c3565
Konstantin <soonts@live.com>
2016-01-09 02:46:17 +0100

    Comments

--- src/Ably.Core/Platform/IPlatform.cs
+++ src/Ably.Core/Platform/IPlatform.cs
@@ -2,6 +2,8 @@
 
 namespace Ably.Platform
 {
+    /// <summary>This interface abstract platform-specific parts of the functionality, hard to implement in this portable library.</summary>
+    /// <remarks>This interface must be implemented by AblyPlatform.PlatformImpl type, from the correct platform-specific AblyPlatform.dll library that must be referenced from your app.</remarks>
     public interface IPlatform
     {
         string getConnectionString();
