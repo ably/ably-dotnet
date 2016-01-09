fc3f0d361a71493881e83b6cbb82ff292e65d74f
Konstantin <soonts@live.com>
2016-01-09 02:32:43 +0100

    iOS and Android platforms (untested)

---
+++ src/Ably.Platforms/Ably.ios/PlatformImpl.cs
@@ -0,0 +1,32 @@
+﻿using Ably.Platform;
+using Ably.Transport;
+using System;
+
+namespace AblyPlatform
+{
+    public class PlatformImpl : IPlatform
+    {
+        public PlatformImpl() { }
+
+        string IPlatform.getConnectionString()
+        {
+            throw new NotSupportedException();
+        }
+
+        ICrypto IPlatform.crypto
+        {
+            get
+            {
+                return new Cryptography.CryptoImpl();
+            }
+        }
+
+        ITransportFactory IPlatform.webSockets
+        {
+            get
+            {
+                return new WebSocketTransport.WebSocketTransportFactory();
+            }
+        }
+    }
+}
