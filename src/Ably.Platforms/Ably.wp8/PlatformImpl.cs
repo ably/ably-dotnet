ffd04efefe1c36a67610a3097a5fedac9d9f00e4
Konstantin <soonts@live.com>
2016-01-09 02:20:43 +0100

    WP8 support (incomplete)

---
+++ src/Ably.Platforms/Ably.wp8/PlatformImpl.cs
@@ -0,0 +1,40 @@
+﻿using Ably.Platform;
+using Ably.Transport;
+using System;
+
+namespace AblyPlatform
+{
+    public class PlatformImpl : IPlatform
+    {
+        public PlatformImpl() { }
+
+        string IPlatform.getConnectionString()
+        {
+            throw new NotSupportedException();
+        }
+
+        ICrypto IPlatform.crypto
+        {
+            get
+            {
+                return new Cryptography.CryptoImpl();
+            }
+        }
+
+        class WebSocketTransportFactory : ITransportFactory
+        {
+            public ITransport CreateTransport( TransportParams parameters )
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        ITransportFactory IPlatform.webSockets
+        {
+            get
+            {
+                return new WebSocketTransportFactory();
+            }
+        }
+    }
+}
