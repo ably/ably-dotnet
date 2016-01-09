687d11a2ca374428445299b70b0218990260d56e
Konstantin <soonts@live.com>
2016-01-09 02:20:43 +0100

    WP8 support (incomplete)

--- src/Ably.Platforms/Ably.wp8/Cryptography/CryptoImpl.cs
+++ src/Ably.Platforms/Ably.wp8/Cryptography/CryptoImpl.cs
@@ -1,23 +1,26 @@
-﻿using Ably.Encryption;
+﻿using Ably;
+using Ably.Encryption;
 using Ably.Platform;
 using System;
 using System.Net;
-using System.Security.Cryptography;
-using Ably;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Security.Cryptography.Core;
 
 namespace AblyPlatform.Cryptography
 {
     internal class CryptoImpl : ICrypto
     {
-        string ICrypto.ComputeHMacSha256( string text, string key )
+        string ICrypto.ComputeHMacSha256( string text, string strKey )
         {
             byte[] bytes = text.GetBytes();
-            byte[] keyBytes = key.GetBytes();
-            using( var hmac = new HMACSHA256( keyBytes ) )
-            {
-                hmac.ComputeHash( bytes );
-                return Convert.ToBase64String( hmac.Hash );
-            }
+            byte[] keyBytes = strKey.GetBytes();
+
+            // TODO: verify it produces same results as that different API in .NET 4
+            MacAlgorithmProvider prov = MacAlgorithmProvider.OpenAlgorithm(MacAlgorithmNames.HmacSha256);
+            CryptographicHash hash = prov.CreateHash(keyBytes.AsBuffer());
+            hash.Append( bytes.AsBuffer() );
+            byte[] hashBytes = hash.GetValueAndReset().ToArray();
+            return Convert.ToBase64String( hashBytes );
         }
 
         IChannelCipher ICrypto.GetCipher( CipherParams p )
@@ -30,7 +33,7 @@ namespace AblyPlatform.Cryptography
 
         CipherParams ICrypto.GetDefaultParams()
         {
-            using( var aes = new AesCryptoServiceProvider() )
+            /* using( var aes = new AesCryptoServiceProvider() )
             {
                 aes.KeySize = Crypto.DefaultKeylength;
                 aes.Mode = System.Security.Cryptography.CipherMode.CBC;
@@ -38,7 +41,8 @@ namespace AblyPlatform.Cryptography
                 aes.BlockSize = Crypto.DefaultBlocklength * 8;
                 aes.GenerateKey();
                 return new CipherParams( aes.Key );
-            }
+            } */
+            throw new NotImplementedException();
         }
     }
 }
