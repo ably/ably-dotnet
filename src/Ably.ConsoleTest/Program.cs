e6289efef098677e0820e61ea8427a6b25a25157
Konstantin <soonts@live.com>
2016-03-08 00:04:20 +0100

    New console test (incomplete)

---
+++ src/Ably.ConsoleTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace IO.Ably.ConsoleTest
+{
+    class Program
+    {
+
+        static void Main( string[] args )
+        {
+            try
+            {
+                mainImpl().Wait();
+                ConsoleColor.Green.writeLine( "Success!" );
+
+            }
+            catch( Exception ex )
+            {
+                ex.logError();
+            }
+        }
+
+        static async Task mainImpl()
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}
