7cf744afc8a5ea6385043d031bf1c10627a4a11c
Konstantin <soonts@live.com>
2016-03-08 22:31:58 +0100

    Test code

--- src/Ably.ConsoleTest/Rest.cs
+++ src/Ably.ConsoleTest/Rest.cs
@@ -1,7 +1,7 @@
-﻿using IO.Ably.Auth;
-using IO.Ably.Rest;
+﻿using IO.Ably.Rest;
 using IO.Ably.Sandbox;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace IO.Ably.ConsoleTest
@@ -29,20 +29,39 @@ namespace IO.Ably.ConsoleTest
 
         public static async Task test()
         {
+            ConsoleColor.DarkGreen.writeLine( "Creating sandbox app.." );
             TestApp sandboxTestData = await AblySandbox.CreateApp();
 
-            ConsoleColor.DarkGreen.writeLine( "Created sandbox app" );
+            ConsoleColor.DarkGreen.writeLine( "Creating REST client.." );
 
             // Create REST client using that key
             AblyRest ably = new AblyRest( sandboxTestData.ToAblyOptions() );
 
-            ConsoleColor.DarkGreen.writeLine( "Created REST client" );
+            ConsoleColor.DarkGreen.writeLine( "Publishing a message.." );
 
             // Verify we can publish
             IChannel channel = ably.Channels.Get( "persisted:presence_fixtures" );
+
+            DateTime tsPublish = DateTime.UtcNow;
             await channel.Publish( "test", true );
 
-            ConsoleColor.DarkGreen.writeLine( "Publish succeeded" );
+            ConsoleColor.DarkGreen.writeLine( "Getting the history.." );
+
+            PaginatedResource<Message> history = await channel.History();
+
+            if( history.Count <= 0 )
+                throw new ApplicationException( "Message lost: not on the history" );
+
+            Message msg = history.First();
+            DateTime tsNow = DateTime.UtcNow;
+            DateTime tsHistory = msg.timestamp.Value;
+
+            if( tsHistory < tsPublish )
+                throw new ApplicationException( "Timestamp's too early. Please ensure your PC's time is correct, use e.g. time.nist.gov server." );
+            if( tsHistory > tsNow )
+                throw new ApplicationException( "Timestamp's too late, Please ensure your PC's time is correct, use e.g. time.nist.gov server." );
+
+            ConsoleColor.DarkGreen.writeLine( "Got the history" );
         }
     }
 }
