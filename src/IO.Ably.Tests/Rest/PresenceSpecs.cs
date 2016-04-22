ef63fb1684f1e75dd78abbb01d4aa493e6092f22
Martin Georgiev <marto83@gmail.com>
2016-04-22 12:54:18 +0100

    Adedd RSP3a

--- src/IO.Ably.Tests/Rest/PresenceSpecs.cs
+++ src/IO.Ably.Tests/Rest/PresenceSpecs.cs
@@ -1,5 +1,6 @@
 using System.Net.Http;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -9,6 +10,7 @@ namespace IO.Ably.Tests
     {
         [Fact]
         [Trait("spec", "RSP1")]
+        [Trait("spec", "RSP3a")]
         public async Task Presence_CreatesGetRequestWithCorrectPath()
         {
             var rest = GetRestClient();
@@ -17,8 +19,16 @@ namespace IO.Ably.Tests
 
             var presence = await channel.Presence.Get();
 
+            presence.Should().BeOfType<PaginatedResource<PresenceMessage>>();
+
             Assert.Equal(HttpMethod.Get, LastRequest.Method);
             Assert.Equal($"/channels/{channel.Name}/presence", LastRequest.Url);
+        }
+
+        [Fact]
+        public async Task FactMethodName()
+        {
+            
         }
 
 
