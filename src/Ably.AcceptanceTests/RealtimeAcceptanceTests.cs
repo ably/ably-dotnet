09980b5e6a2669f7822f8c908251e4d4683a5d2f
Martin Georgiev <marto83@gmail.com>
2016-05-06 12:28:12 +0100

    Removed redundant tests

--- src/Ably.AcceptanceTests/RealtimeAcceptanceTests.cs
+++ src/Ably.AcceptanceTests/RealtimeAcceptanceTests.cs
@@ -31,85 +31,6 @@ namespace IO.Ably.AcceptanceTests
         }
 
         [Test]
-        public void TestCanConnectToAbly()
-        {
-            // Act
-
-            Assert.True(false); //Fail the test until properly exposed
-        }
-
-        [Test]
-        public void TestCreateRealtimeClient_IsAutoConnecting()
-        {
-            // Act
-            var client = GetRealtimeClient();
-
-            // Assert
-            client.Connection.State.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connecting);
-            client.Connection.Close();
-        }
-
-        [Test]
-        public void TestCreateRealtimeClient_ConnectsSuccessfuly()
-        {
-            // Act
-            var client = GetRealtimeClient();
-            AutoResetEvent signal = new AutoResetEvent(false);
-            var args = new List<Realtime.ConnectionStateChangedEventArgs>();
-
-            client.Connection.ConnectionStateChanged += (s, e) =>
-            {
-                args.Add(e);
-                signal.Set();
-            };
-
-            // Assert
-            signal.WaitOne(10000);
-
-            args.Count.ShouldBeEquivalentTo(1);
-            args[0].CurrentState.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connected);
-            args[0].PreviousState.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connecting);
-            args[0].Reason.ShouldBeEquivalentTo(null);
-            client.Connection.State.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connected);
-        }
-
-        [Test]
-        public async Task TestCreateRealtimeClient_AutoConnect_False_ConnectsSuccessfuly()
-        {
-            // Arrange
-            var client = GetRealtimeClient(o => o.AutoConnect = false);
-            AutoResetEvent signal = new AutoResetEvent(false);
-            var args = new List<Realtime.ConnectionStateChangedEventArgs>();
-
-            client.Connection.ConnectionStateChanged += (s, e) =>
-            {
-                args.Add(e);
-                if (args.Count == 2) signal.Set();
-            };
-
-            // Act
-            client.Connection.State.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Initialized);
-            client.Connect();
-
-            // Assert
-            client.Connection.State.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connecting);
-
-            args.Count.ShouldBeEquivalentTo(1);
-            args[0].PreviousState.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Initialized);
-            args[0].CurrentState.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connecting);
-            args[0].Reason.ShouldBeEquivalentTo(null);
-
-            signal.WaitOne(10000);
-
-            args.Count.ShouldBeEquivalentTo(2);
-            args[1].PreviousState.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connecting);
-            args[1].CurrentState.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connected);
-            args[1].Reason.ShouldBeEquivalentTo(null);
-
-            client.Connection.State.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connected);
-        }
-
-        [Test]
         public void TestCreateRealtimeClient_DisconnectsSuccessfuly()
         {
             // Arrange
@@ -211,45 +132,6 @@ namespace IO.Ably.AcceptanceTests
         }
 
         [Test]
-        public async Task TestRealtimeConnectionID_MultipleClientsHaveDifferentIds()
-        {
-            const int clientCount = 5;
-
-            // Arrange
-            HashSet<string> ids = new HashSet<string>();
-            Semaphore signal = new Semaphore(0, clientCount);
-
-            // Act
-            for (int i = 0; i < clientCount; i++)
-            {
-                var client = GetRealtimeClient(o => o.AutoConnect = false);
-
-                client.Connection.ConnectionStateChanged += (s, e) =>
-                {
-                    if (e.CurrentState == Realtime.ConnectionStateType.Connected)
-                    {
-                        lock (ids)
-                        {
-                            ids.Add(client.Connection.Id);
-                        }
-                        signal.Release();
-                    }
-                };
-
-                client.Connect();
-            }
-
-            // wait for all to complete
-            for (int i = 0; i < clientCount; i++)
-            {
-                signal.WaitOne(10000);
-            }
-
-            // Assert
-            ids.Count.ShouldBeEquivalentTo(clientCount);
-        }
-
-        [Test]
         public async Task TestRealtimeConnectionKey_IsNullUntilConnected()
         {
             // Arrange
@@ -271,72 +153,7 @@ namespace IO.Ably.AcceptanceTests
             Assert.IsNull(keyBeforeConnect);
             Assert.IsNotNull(client.Connection.Key);
         }
-
-        [Test]
-        public async Task TestRealtimeConnectionKey_MultipleClientsHaveDifferentKeys()
-        {
-            const int clientCount = 5;
-
-            // Arrange
-            HashSet<string> keys = new HashSet<string>();
-            Semaphore signal = new Semaphore(0, clientCount);
-
-            // Act
-            for (int i = 0; i < clientCount; i++)
-            {
-                var client = GetRealtimeClient(o => o.AutoConnect = false);
-
-                client.Connection.ConnectionStateChanged += (s, e) =>
-                {
-                    if (e.CurrentState == Realtime.ConnectionStateType.Connected)
-                    {
-                        lock (keys)
-                        {
-                            keys.Add(client.Connection.Key);
-                        }
-                        signal.Release();
-                    }
-                };
-
-                client.Connect();
-            }
-
-            // wait for all to complete
-            for (int i = 0; i < clientCount; i++)
-            {
-                signal.WaitOne(10000);
-            }
-
-            // Assert
-            keys.Count.ShouldBeEquivalentTo(clientCount);
-        }
-
-        [Test]
-        public void TestRealtimeClient_InvalidApiKey_GoesFailed()
-        {
-            // Act
-            var client = GetRealtimeClient(o => o.Key = "123.456:789");
-            AutoResetEvent signal = new AutoResetEvent(false);
-            var args = new List<Realtime.ConnectionStateChangedEventArgs>();
-
-            client.Connection.ConnectionStateChanged += (s, e) =>
-            {
-                args.Add(e);
-                signal.Set();
-            };
-
-            // Assert
-            signal.WaitOne(10000);
-
-            args.Count.ShouldBeEquivalentTo(1);
-            args[0].CurrentState.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Failed);
-            args[0].PreviousState.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Connecting);
-            args[0].Reason.ShouldBeEquivalentTo(client.Connection.Reason);
-            client.Connection.State.ShouldBeEquivalentTo(Realtime.ConnectionStateType.Failed);
-            client.Connection.Reason.code.ShouldBeEquivalentTo(40400);
-            client.Connection.Reason.statusCode.ShouldBeEquivalentTo(System.Net.HttpStatusCode.NotFound);
-        }
-
+        
         [Test]
         public void TestRealtimeClient_ConnectionSerialIsMinus1WhenConnected()
         {
