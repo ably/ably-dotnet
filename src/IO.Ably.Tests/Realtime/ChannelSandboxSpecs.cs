8229060b2a7c15775deb3ddeef2398f3dd1b8c0a
Martin Georgiev <marto83@gmail.com>
2016-05-17 22:08:28 +0100

    Moved All acceptance tests to IO.Ably.Specs and removed the other test project

---
+++ src/IO.Ably.Tests/Realtime/ChannelSandboxSpecs.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using IO.Ably.Realtime;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IO.Ably.Tests.Realtime
+{
+    [Collection("AblyRest SandBox Collection")]
+    [Trait("requires", "sandbox")]
+    public class ChannelSandboxSpecs : SandboxSpecs
+    {
+        [Theory]
+        [ProtocolData]
+        public async Task TestGetChannel_ReturnsValidChannel(Protocol protocol)
+        {
+            // Arrange
+            var client = await GetRealtimeClient(protocol);
+
+            // Act
+            IRealtimeChannel target = client.Channels.Get("test");
+
+            // Assert
+            target.Name.ShouldBeEquivalentTo("test");
+            target.State.ShouldBeEquivalentTo(ChannelState.Initialized);
+        }
+
+        [Theory]
+        [ProtocolData]
+        public async Task TestAttachChannel_AttachesSuccessfuly(Protocol protocol)
+        {
+            // Arrange
+            var client = await GetRealtimeClient(protocol);
+            Semaphore signal = new Semaphore(0, 2);
+            var args = new List<ChannelStateChangedEventArgs>();
+            IRealtimeChannel target = client.Channels.Get("test");
+            target.ChannelStateChanged += (s, e) =>
+            {
+                args.Add(e);
+                signal.Release();
+            };
+
+            // Act
+            target.Attach();
+
+            // Assert
+            signal.WaitOne(10000);
+            args.Count.ShouldBeEquivalentTo(1);
+            args[0].NewState.ShouldBeEquivalentTo(ChannelState.Attaching);
+            args[0].Reason.ShouldBeEquivalentTo(null);
+            target.State.ShouldBeEquivalentTo(ChannelState.Attaching);
+
+            signal.WaitOne(10000);
+            args.Count.ShouldBeEquivalentTo(2);
+            args[1].NewState.ShouldBeEquivalentTo(ChannelState.Attached);
+            args[1].Reason.ShouldBeEquivalentTo(null);
+            target.State.ShouldBeEquivalentTo(ChannelState.Attached);
+        }
+
+        [Theory]
+        [ProtocolData]
+        public async Task TestAttachChannel_SendingMessage_EchoesItBack(Protocol protocol)
+        {
+            // Arrange
+            var client = await GetRealtimeClient(protocol);
+            AutoResetEvent signal = new AutoResetEvent(false);
+            IRealtimeChannel target = client.Channels.Get("test");
+            target.Attach();
+            List<Message> messagesReceived = new List<Message>();
+            target.Subscribe(messages =>
+            {
+                messagesReceived.AddRange(messages);
+                signal.Set();
+            });
+
+            // Act
+            target.Publish("test", "test data");
+            signal.WaitOne(10000);
+
+            // Assert
+            messagesReceived.Count.ShouldBeEquivalentTo(1);
+            messagesReceived[0].name.ShouldBeEquivalentTo("test");
+            messagesReceived[0].data.ShouldBeEquivalentTo("test data");
+        }
+
+        [Theory]
+        [ProtocolData]
+        public async Task TestAttachChannel_Sending3Messages_EchoesItBack(Protocol protocol)
+        {
+            // Arrange
+            var client = await GetRealtimeClient(protocol);
+            AutoResetEvent signal = new AutoResetEvent(false);
+            IRealtimeChannel target = client.Channels.Get("test");
+            target.Attach();
+            List<Message> messagesReceived = new List<Message>();
+            target.Subscribe(messages =>
+            {
+                messagesReceived.AddRange(messages);
+                signal.Set();
+            });
+
+            // Act
+            target.Publish("test1", "test 12");
+            target.Publish("test2", "test 123");
+            target.Publish("test3", "test 321");
+            signal.WaitOne(10000);
+
+            // Assert
+            messagesReceived.Count.ShouldBeEquivalentTo(3);
+            messagesReceived[0].name.ShouldBeEquivalentTo("test1");
+            messagesReceived[0].data.ShouldBeEquivalentTo("test 12");
+            messagesReceived[1].name.ShouldBeEquivalentTo("test2");
+            messagesReceived[1].data.ShouldBeEquivalentTo("test 123");
+            messagesReceived[2].name.ShouldBeEquivalentTo("test3");
+            messagesReceived[2].data.ShouldBeEquivalentTo("test 321");
+        }
+
+        [Theory]
+        [ProtocolData]
+        public async Task TestAttachChannel_SendingMessage_Doesnt_EchoesItBack(Protocol protocol)
+        {
+            // Arrange
+            var client = await GetRealtimeClient(protocol, o => o.EchoMessages = false);
+            AutoResetEvent signal = new AutoResetEvent(false);
+            var target = client.Channels.Get("test");
+
+            target.Attach();
+
+            List<Message> messagesReceived = new List<Message>();
+            target.Subscribe(messages =>
+            {
+                messagesReceived.AddRange(messages);
+                signal.Set();
+            });
+
+            // Act
+            target.Publish("test", "test data");
+            signal.WaitOne(10000);
+
+            // Assert
+            messagesReceived.Count.ShouldBeEquivalentTo(0);
+        }
+
+        public ChannelSandboxSpecs(AblySandboxFixture fixture, ITestOutputHelper output) : base(fixture, output)
+        {
+        }
+    }
+}
--- src/IO.Ably.Tests/Rest/StatsSpecs.cs
+++ src/IO.Ably.Tests/Rest/StatsSpecs.cs
@@ -1,11 +1,134 @@
+using System;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
 using static Xunit.Assert;
 
 namespace IO.Ably.Tests
 {
+    [Collection("AblyRest SandBox Collection")]
+    [Trait("requires", "sandbox")]
+    public class StatsSandBoxSpecs : SandboxSpecs
+    {
+        public readonly static DateTimeOffset StartInterval = DateHelper.CreateDate(DateTimeOffset.UtcNow.Year - 1, 2, 3, 15, 5);
+
+        //Stats fixtures can be found in StatsFixture.json which is posted to /stats in TestsSetup.cs
+
+        private readonly Protocol _protocol;
+
+        private AblyRest GetAbly()
+        {
+            var testData = TestsSetup.TestData;
+
+            var options = new ClientOptions
+            {
+                Key = testData.keys.First().keyStr,
+                UseBinaryProtocol = _protocol == Protocol.MsgPack,
+                Environment = AblyEnvironment.Sandbox
+            };
+            var ably = new AblyRest(options);
+            return ably;
+        }
+
+        [TestFixture(Protocol.Json)]
+        [TestFixture(Protocol.MsgPack)]
+        //[Ignore("Ignoring for the time being. Will fix when I get to stats")]
+        public class ByMinuteWhenFromSetToStartIntervalAndLimitSetTo1 : StatsAcceptanceTests
+        {
+            public ByMinuteWhenFromSetToStartIntervalAndLimitSetTo1(Protocol protocol) : base(protocol)
+            {
+            }
+
+            [OneTimeSetUp]
+            public void GetStats()
+            {
+                var client = GetAbly();
+                Stats = client.Stats(new StatsDataRequestQuery() { Start = StartInterval.AddMinutes(-30), Limit = 1 }).Result;
+
+                TestStats = Stats.First();
+            }
+
+            public PaginatedResult<Stats> Stats { get; set; }
+            public Stats TestStats { get; set; }
+
+            [Fact]
+            public void RetrievesOnlyOneStat()
+            {
+                Stats.Should().HaveCount(1);
+            }
+
+            [Fact]
+            public void ShouldReturnCorrectAggregatedMessageData()
+            {
+                TestStats.All.Messages.Count.Should().Be(40 + 70);
+                TestStats.All.Messages.Data.Should().Be(4000 + 7000);
+            }
+
+            [Fact]
+            public void ReturnsAccurateInboundRealtimeAllData()
+            {
+                TestStats.Inbound.Realtime.All.Count.Should().Be(70);
+                TestStats.Inbound.Realtime.All.Data.Should().Be(7000);
+            }
+
+            [Fact]
+            public void ReturnsAccurateInboundRealtimeMessageData()
+            {
+                TestStats.Inbound.Realtime.Messages.Count.Should().Be(70);
+                TestStats.Inbound.Realtime.Messages.Data.Should().Be(7000);
+            }
+
+            [Fact]
+            public void ReturnsAccurateOutboundRealtimeAllData()
+            {
+                TestStats.Outbound.Realtime.All.Count.Should().Be(40);
+                TestStats.Outbound.Realtime.All.Data.Should().Be(4000);
+            }
+
+            [Fact]
+            public void ReturnsAccuratePersistedPresenceAllData()
+            {
+                TestStats.Persisted.Presence.Count.Should().Be(20);
+                TestStats.Persisted.Presence.Data.Should().Be(2000);
+            }
+
+            [Fact]f
+            public void ReturnsAccurateConnectionsAllData()
+            {
+                TestStats.Connections.Tls.Peak.Should().Be(20);
+                TestStats.Connections.Tls.Opened.Should().Be(10);
+            }
+
+            [Fact]
+            public void ReturnsAccurateChannelsAllData()
+            {
+                TestStats.Channels.Peak.Should().Be(50);
+                TestStats.Channels.Opened.Should().Be(30);
+            }
+
+            [Fact]
+            public void ReturnsAccurentApiRequestsData()
+            {
+                TestStats.ApiRequests.Succeeded.Should().Be(50);
+                TestStats.ApiRequests.Failed.Should().Be(10);
+            }
+
+            [Fact]
+            public void ReturnsAccurateTokenRequestsData()
+            {
+                TestStats.TokenRequests.Succeeded.Should().Be(60);
+                TestStats.TokenRequests.Failed.Should().Be(20);
+            }
+        }
+
+        public StatsSandBoxSpecs(AblySandboxFixture fixture, ITestOutputHelper output) : base(fixture, output)
+        {
+        }
+    }
+
     public class StatsSpecs : MockHttpRestSpecs
     {
         [Fact]
