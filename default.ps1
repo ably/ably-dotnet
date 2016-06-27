d583aa184b9a57772bd13b6085b86d447c99862a
Martin Georgiev <marto83@gmail.com>
2016-06-27 13:54:04 +0100

    Bumped the version

--- default.ps1
+++ default.ps1
@@ -10,7 +10,7 @@ properties {
 	$sln_name = "IO.Ably.sln"
 	
 	$project_name = "IO.Ably"
-	$build_number = "0.8.2-Beta"
+	$build_number = "0.8.3"
 	$solution_dir = "$build_script_dir\$sln_dir"
 	
 	$build_artifacts_dir_base = "$build_script_dir\build\artifacts"
