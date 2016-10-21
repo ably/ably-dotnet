649d916088290e57b5b142bed6772b4d4957a828
Martin Georgiev <marto83@gmail.com>
2016-10-21 12:21:27 +0100

    Added extra logging to the HttpClient

--- package.ps1
+++ package.ps1
@@ -1,3 +1,3 @@
 C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\msbuild.exe /property:Configuration=Release .\\src\\Ably.sln
 C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\msbuild.exe /property:Configuration="Release 4.0" .\\src\\Ably.sln
-.\\tools\\NuGet.exe pack .\\nuget\\ably.io.nuspec
+.\\tools\\NuGet.exe pack .\\nuget\\io.ably.nuspec
