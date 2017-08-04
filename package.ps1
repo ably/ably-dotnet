a79235eff66dc508a1c1f906d0934de2c3a21d7c
Martin Georgiev <marto83@gmail.com>
2017-08-04 01:21:21 +0100

    Updated Samples .net framework.

--- package.ps1
+++ package.ps1
@@ -1,7 +1,7 @@
 import-module .\tools\psake\psake.psm1
 
 $psake.use_exit_on_error = $true
-invoke-psake ./default.ps1 Build -properties @{ configuration = "package"; sln_name = "IO.Ably.Package.sln" } -framework '4.0' 
+#invoke-psake ./default.ps1 Build -properties @{ configuration = "package"; sln_name = "IO.Ably.Package.sln" } -framework '4.0' 
 
 remove-module psake
 
