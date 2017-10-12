fcd56f7ebdd9eecd0cf39c5ddb584a57dd41869b
Jack Rutherford <jack@fader.co.uk>
2017-10-12 17:07:59 +0100

    appveyor specific build script

---
+++ build_appveyor.ps1
@@ -0,0 +1,12 @@
+cd tools\psake
+import-module .\psake.psm1
+cd..
+cd..
+
+$psake.use_exit_on_error = $true
+invoke-psake ./default.ps1 -properties @{ configuration = "release", msbuild = "msbuild" } -framework '4.0' -verbose
+
+cd tools\psake
+remove-module psake
+cd..
+cd..
