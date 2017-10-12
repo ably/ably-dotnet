a04b9c6f2b3fbd41167f7208302eb8acebf31abb
Jack Rutherford <jack@fader.co.uk>
2017-10-12 17:11:37 +0100

    fix syntax error

--- build_appveyor.ps1
+++ build_appveyor.ps1
@@ -4,7 +4,7 @@ cd..
 cd..
 
 $psake.use_exit_on_error = $true
-invoke-psake ./default.ps1 -properties @{ configuration = "release", msbuild = "msbuild" } -framework '4.0' -verbose
+invoke-psake ./default.ps1 -properties @{ configuration = "release"; msbuild = "msbuild" } -framework '4.0' -verbose
 
 cd tools\psake
 remove-module psake
