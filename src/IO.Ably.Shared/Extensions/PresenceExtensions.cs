4baa3283372bca5c994694db678343b43f04f80c
Jed Dawson <jed@edisoncode.com>
2017-10-12 06:26:56 -0700

    Added extensions and made PresenceMessages comparable to each other

---
+++ src/IO.Ably.Shared/Extensions/PresenceExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace IO.Ably
+{
+    internal static class PresenceExtensions
+    {
+        public static bool IsSynthesized(this PresenceMessage msg)
+        {
+            return !msg.Id.StartsWith(msg.ConnectionId);
+        }
+
+        public static bool IsNewerThan(this PresenceMessage oldMessage, PresenceMessage newMessage)
+        {
+            return oldMessage.CompareTo(newMessage) > 0;
+        }
+    }
+}
