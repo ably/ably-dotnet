17d7a75aabb065ab0a23bd91675a94b13c1ebc9d
Jack Rutherford <jack@fader.co.uk>
2017-10-12 20:36:27 +0100

    force path to MSBuild

--- build_appveyor.ps1
+++ build_appveyor.ps1
@@ -4,7 +4,7 @@ cd..
 cd..
 
 $psake.use_exit_on_error = $true
-invoke-psake ./default.ps1 -properties @{ configuration = "ci_release"; msbuild = "msbuild" } -framework '4.0' -verbose
+invoke-psake ./default.ps1 -properties @{ configuration = "ci_release"; msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" } -framework '4.0' -verbose
 
 cd tools\psake
 remove-module psake
