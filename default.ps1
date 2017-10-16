a501cf2751c9d5bce5605130075870ce4dd9da6d
Jack Rutherford <jack@fader.co.uk>
2017-10-16 20:35:26 +0100

    clean up debug messages

--- default.ps1
+++ default.ps1
@@ -82,14 +82,10 @@ task Unit_Tests {
 			Remove-Item $sandbox_test_results -Force
 		}
 
-		Write-Host $packages_dir
-
 		$xunit_console = Get-ChildItem -Path ".\src\packages\" -Filter xunit.console.exe -Recurse -ErrorAction SilentlyContinue -Force | Select FullName | Select-Object -First 1 | Select -ExpandProperty FullName
 		if($xunit_console -eq "") {
 			$xunit_console = "$tools_dir\xunit\xunit.console.exe"
 		}
-		Write-Host "Using xunit.console at path '$xunit_console'"
-		Write-Host "$xunit_console $testDll -noappdomain -nunit $unit_test_results -notrait requires=sandbox"
 		#Run all the unit tests first
 		& "$xunit_console" $testDll -noappdomain -nunit "$unit_test_results" -notrait "requires=sandbox"
 		#& "$xunit_console" $testDll -diagnostics -nunit "$sandbox_test_results" -trait "requires=sandbox"
