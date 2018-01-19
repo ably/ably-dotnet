758ea1f472abd266e1883df0463395140cb11006
Jack Rutherford <jack@fader.co.uk>
2018-01-19 17:08:32 +0000

    added NetStandard tests

---
+++ src/IO.Ably.NETStandard20/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IO.Ably")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+#if !PACKAGE
+[assembly: InternalsVisibleTo("IO.Ably.Tests.NetStandard20")]
+#endif
