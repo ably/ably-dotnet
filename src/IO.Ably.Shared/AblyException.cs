b5418334f8cc54d0d11bddcf611e94296a77e9db
Jack Rutherford <jack@fader.co.uk>
2018-03-23 06:49:05 +0000

    cleaned up AblyException, removed commented out code and cleaned up XML docs. Reorganised and added to constructor overloads to reduce the need to create new ErrorInfo objects that are passed in.

--- src/IO.Ably.Shared/AblyException.cs
+++ src/IO.Ably.Shared/AblyException.cs
@@ -12,15 +12,13 @@ namespace IO.Ably
     }
 
     /// <summary>
-    /// Ably exception wrapper class. It includes error information <see cref="Ably.ErrorInfo"/> used by ably.
-    /// All inner exceptions are wrapped in this class. Always check the inner exception property of the caught exception.
+    /// Ably exception wrapper class. It includes error information (<see cref="Ably.ErrorInfo"/>) used by Ably.
+    /// All inner exceptions are wrapped in this class.
+    /// Always check the inner exception property of the caught exception.
     /// </summary>
     public class AblyException : Exception
     {
-        /* public override void GetObjectData(SerializationInfo info, StreamingContext context)
-        {
-            base.GetObjectData(info, context);
-        } */
+        /// <inheritdoc />
         public AblyException()
         {
         }
@@ -36,35 +34,45 @@ namespace IO.Ably
         }
 
         /// <summary>
+        /// Initializes a new instance of the <see cref="AblyException"/> class.
         /// Creates AblyException. ErrorInfo is automatically generated based on the inner exception message. StatusCode is set to 50000
         /// </summary>
         /// <param name="ex">Original exception to be wrapped.</param>
         public AblyException(Exception ex)
             : this(new ErrorInfo("Unexpected error :" + ex.Message, 50000), ex)
-            {
-            }
-
-        /// <summary>
-        /// Creates an AblyException and populates ErrorInfo with the supplied parameters.
-        /// </summary>
-        /// <param name="reason"></param>
-        /// <param name="code"></param>
-        public AblyException(string reason, int code)
-            : this(new ErrorInfo(reason, code))
         {
         }
 
+
         /// <summary>
-        /// Creates AblyException and populates ErrorInfo with the supplied parameters.
+        /// Initializes a new instance of the <see cref="AblyException"/> class.
         /// </summary>
+        /// <param name="reason">A brief, helpful message explaining the reason for the error</param>
+        /// <param name="code">The Ably code (see spec item T11)</param>
+        /// <param name="statusCode">Optional, analogous to HTTP status code</param>
         public AblyException(string reason, int code, HttpStatusCode? statusCode = null)
             : this(new ErrorInfo(reason, code, statusCode))
         {
         }
 
         /// <summary>
-        /// Creates AblyException with supplied error info.
+        /// Initializes a new instance of the <see cref="AblyException"/> class with <see cref="ErrorInfo"/> set.
+        /// Configures ErrorInfo using the supplied parameters.
         /// </summary>
+        /// <param name="reason">A brief, helpful message explaining the reason for the error</param>
+        /// <param name="code">The Ably code (see spec item T11)</param>
+        /// <param name="statusCode">Optional, analogous to HTTP status code</param>
+        /// <param name="innerException">The underlying exception</param>
+        public AblyException(string reason, int code, HttpStatusCode? statusCode, Exception innerException)
+            : this(new ErrorInfo(reason, code, statusCode), innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AblyException"/> class.
+        /// Creates AblyException with supplied <see cref="ErrorInfo"/>.
+        /// </summary>
+        /// <param name="info">An instance of <see cref="ErrorInfo"/></param>
         public AblyException(ErrorInfo info)
             : base(info.ToString())
         {
@@ -72,19 +80,19 @@ namespace IO.Ably
         }
 
         /// <summary>
-        /// Creates AblyException with ErrorInfo and sets the supplied exception as innerException
+        /// Initializes a new instance of the <see cref="AblyException"/> class with <see cref="ErrorInfo"/> and sets the supplied exception as innerException
         /// </summary>
+        /// <param name="info">An instance of <see cref="ErrorInfo"/></param>
+        /// <param name="innerException">The underlying exception</param>
         public AblyException(ErrorInfo info, Exception innerException)
             : base(info.ToString(), innerException)
         {
             ErrorInfo = info;
         }
 
-        /* protected AblyException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-
-        } */
+        /// <summary>
+        /// Get and set <see cref="ErrorInfo"/>
+        /// </summary>
         public ErrorInfo ErrorInfo { get; set; }
 
         internal static AblyException FromResponse(AblyResponse response)
