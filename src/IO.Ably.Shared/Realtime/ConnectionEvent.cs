9c7f73e3bbec8e9404987e233ad48329acfcdc18
Jack Rutherford <jack@fader.co.uk>
2018-04-25 22:23:09 +0100

    add ConnectionEvent enum

---
+++ src/IO.Ably.Shared/Realtime/ConnectionEvent.cs
@@ -0,0 +1,19 @@
+﻿namespace IO.Ably.Realtime
+{
+    /// <summary>A series of connection states.</summary>
+    public enum ConnectionEvent
+    {
+        /*
+         The values assigned to each enum should correspond with those assigned in ConnectionState.
+         */
+        Initialized = 0,
+        Connecting = 1,
+        Connected = 2,
+        Disconnected = 3,
+        Suspended = 4,
+        Closing = 5,
+        Closed = 6,
+        Failed = 7,
+        Update = 8
+    }
+}
