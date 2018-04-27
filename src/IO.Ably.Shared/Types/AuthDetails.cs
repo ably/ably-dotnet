baade42f4829c135b4dfa4ef7fcbf83724f518d4
Jack Rutherford <jack@fader.co.uk>
2018-04-27 10:53:43 +0100

    adding AuthDetails class

---
+++ src/IO.Ably.Shared/Types/AuthDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace IO.Ably.Types
+{
+    /// <summary>
+    /// AuthDetails is a type used with an AUTH protocol messages to send authentication details
+    /// </summary>
+    public class AuthDetails
+    {
+        /// <summary>
+        /// Gets or sets the accessToken.
+        /// </summary>
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; set; }
+    }
+}
