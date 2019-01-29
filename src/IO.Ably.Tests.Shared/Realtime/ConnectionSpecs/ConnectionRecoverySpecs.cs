bed5f8daee777c28cb3558939b8212d6dfd6228a
Jack Rutherford <jack@fader.co.uk>
2019-01-29 11:37:21 +0000

    test for RTN16f

--- src/IO.Ably.Tests.Shared/Realtime/ConnectionSpecs/ConnectionRecoverySpecs.cs
+++ src/IO.Ably.Tests.Shared/Realtime/ConnectionSpecs/ConnectionRecoverySpecs.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using FluentAssertions;
 using IO.Ably.Realtime;
+using IO.Ably.Transport;
 using IO.Ably.Types;
 using Xunit;
 using Xunit.Abstractions;
@@ -29,8 +30,48 @@ namespace IO.Ably.Tests.Realtime.ConnectionSpecs
         public void RecoveryKey_ShouldBeConnectionKeyPlusConnectionSerialPlusMsgSerial()
         {
             var client = GetConnectedClient();
-            client.Connection.Serial.Should().Be()
             client.Connection.RecoveryKey.Should().Be($"{client.Connection.Key}:{client.Connection.Serial}:{client.Connection.MessageSerial}");
+        }
+
+        [Fact]
+        [Trait("spec", "RTN16f")]
+        public async Task RecoveryKey_MsgSerialShouldNotBeSentToAblyButShouldBeSetOnConnection()
+        {
+            // RecoveryKey should be in the format
+            // LettersOrNumbers:Number:Number
+            TransportParams.RecoveryKeyRegex.Match("a:b:c").Success.Should().BeFalse();
+            TransportParams.RecoveryKeyRegex.Match("a:b:3").Success.Should().BeFalse();
+            TransportParams.RecoveryKeyRegex.Match("a:2:c").Success.Should().BeFalse();
+            TransportParams.RecoveryKeyRegex.Match("$1:2:3").Success.Should().BeFalse();
+            TransportParams.RecoveryKeyRegex.Match("$a:2:3").Success.Should().BeFalse();
+            TransportParams.RecoveryKeyRegex.Match("a:@2:3").Success.Should().BeFalse();
+            TransportParams.RecoveryKeyRegex.Match("a:2:3!").Success.Should().BeFalse();
+
+            // these should be valid
+            TransportParams.RecoveryKeyRegex.Match("1:2:3").Success.Should().BeTrue();
+            TransportParams.RecoveryKeyRegex.Match("a:2:3").Success.Should().BeTrue();
+
+            var recoveryKey = "abcxyz:100:99";
+            var match = TransportParams.RecoveryKeyRegex.Match(recoveryKey);
+            match.Success.Should().BeTrue();
+            match.Groups[1].Value.Should().Be("abcxyz");
+            match.Groups[2].Value.Should().Be("100");
+            match.Groups[3].Value.Should().Be("99");
+
+            var parts = recoveryKey.Split(':');
+
+            var client = GetRealtimeClient(options => { options.Recover = recoveryKey; });
+
+            var transportParams = await client.ConnectionManager.CreateTransportParameters();
+            var paramsDict = transportParams.GetParams();
+            paramsDict.ContainsKey("recover").Should().BeTrue();
+            paramsDict.ContainsKey("connection_serial").Should().BeTrue();
+            paramsDict.ContainsKey("msg_serial").Should().BeFalse();
+
+            paramsDict["recover"].Should().Be(parts[0]);
+            paramsDict["connection_serial"].Should().Be(parts[1]);
+
+            client.Connection.MessageSerial.Should().Be(99);
         }
 
         public ConnectionRecoverySpecs(ITestOutputHelper output)
