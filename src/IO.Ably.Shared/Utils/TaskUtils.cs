3faa0f02ec0bbc094b66a632ac3af24db68f9ae4
Jack Rutherford <jack@fader.co.uk>
2019-04-24 16:48:31 +0100

    added helper to run task in background

--- src/IO.Ably.Shared/Utils/TaskUtils.cs
+++ src/IO.Ably.Shared/Utils/TaskUtils.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 
 namespace IO.Ably
 {
@@ -40,6 +41,36 @@ namespace IO.Ably
                         return res.Task;
                     },
                 TaskContinuationOptions.ExecuteSynchronously).Unwrap();
+        }
+
+        private static readonly Action<Task> DefaultErrorContinuation =
+            t =>
+            {
+                try { t.Wait(); }
+                catch { }
+            };
+
+        public static void RunInBackground(Action action, Action<Exception> handler = null)
+        {
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            var task = Task.Run(action);  // Adapt as necessary for .NET 4.0.
+
+            if (handler == null)
+            {
+                task.ContinueWith(
+                    DefaultErrorContinuation,
+                    TaskContinuationOptions.ExecuteSynchronously |
+                    TaskContinuationOptions.OnlyOnFaulted);
+            }
+            else
+            {
+                task.ContinueWith(
+                    t => handler(t.Exception.GetBaseException()),
+                    TaskContinuationOptions.ExecuteSynchronously |
+                    TaskContinuationOptions.OnlyOnFaulted);
+            }
         }
     }
 }
