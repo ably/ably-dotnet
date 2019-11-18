0cf8c280e615a781bbcd2aaebbdad11e9507b9b9
Martin Georgiev <marto83@gmail.com>
2019-11-18 11:26:40 +0000

    Hook up AblyCodecEncoder to actually call the underlying AblyCodec
    I made sure we handle exceptions from the AblyCodec. Later we need to hook up any recovery logic here

--- src/IO.Ably.Shared/MessageEncoders/AblyCodecEncoder.cs
+++ src/IO.Ably.Shared/MessageEncoders/AblyCodecEncoder.cs
@@ -5,8 +5,9 @@ namespace IO.Ably.MessageEncoders
     internal class AblyCodecEncoder : MessageEncoder
     {
         private readonly IAblyCodec _codecEncoder;
+        private readonly ILogger _logger;
 
-        public AblyCodecEncoder(string encodingName, IAblyCodec codecEncoder)
+        public AblyCodecEncoder(string encodingName, IAblyCodec codecEncoder, ILogger logger)
         {
             if (encodingName.IsEmpty())
             {
@@ -20,6 +21,7 @@ namespace IO.Ably.MessageEncoders
 
             EncodingName = encodingName;
             _codecEncoder = codecEncoder;
+            _logger = logger;
         }
 
         public override string EncodingName { get; }
@@ -36,7 +38,27 @@ namespace IO.Ably.MessageEncoders
 
         public override Result<ProcessedPayload> Decode(IPayload payload, EncodingDecodingContext context)
         {
-            return Result.Ok(new ProcessedPayload(payload));
+            try
+            {
+                _logger.Debug($"CustomCodec - EncodingName: {EncodingName}. AblyCodec: {_codecEncoder.GetType().Name}");
+                var decodedPayload = _codecEncoder.Decode(payload.Data, context);
+                return Result.Ok(new ProcessedPayload()
+                {
+                    Data = decodedPayload,
+                    Encoding = RemoveCurrentEncodingPart(payload),
+                });
+            }
+            catch (AblyException ablyEx)
+            {
+                ablyEx.ErrorInfo.InnerException = ablyEx;
+                return Result.Fail<ProcessedPayload>(ablyEx.ErrorInfo);
+            }
+            catch (Exception e)
+            {
+                return Result.Fail<ProcessedPayload>(
+                    new ErrorInfo($"Error decoding Payload with encoding: {payload.Encoding} using AblyCodec {_codecEncoder.GetType().Name}")
+                        { InnerException = e });
+            }
         }
     }
 }
