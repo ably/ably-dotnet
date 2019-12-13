558f025fbff07213505230d1afe95a8b0a25d7f6
Martin Georgiev <marto83@gmail.com>
2019-12-13 15:46:25 +0000

    Add tests for spec items: RSL6d1 and RSL6d2
    ** @(RSL6d)@ The data payload of the last message on each channel must be stored at all times since it will be needed to decode any subsequent message that has a "vcdiff" encoding step. The stored value is the "base payload" of the most recent message; this is the @data@ member of the message, in string or binary form, once all application-level encoding steps have been applied. The base payload is derived initially by processing a non-delta message; the processing and bookkeeping rules are as follows:
    *** @(RSL6d1)@ When processing any message (whether a delta or a full message), if the message @encoding@ string ends in @base64@, the message @data@ should be base64-decoded (and the encoding string modified accordingly per "RSL6":#RSL6).
    *** @(RSL6d2)@ In the case of a non-delta message, the resulting @data@ value is stored as the base payload.

---
+++ src/IO.Ably.Tests.Shared/Realtime/DeltaLastMessageSpecs.cs
@@ -0,0 +1,88 @@
+using System.Threading.Tasks;
+using FluentAssertions;
+using IO.Ably.Realtime;
+using IO.Ably.Realtime.Workflow;
+using IO.Ably.Tests.Infrastructure;
+using IO.Ably.Types;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IO.Ably.Tests.DotNetCore20.Realtime
+{
+    [Trait("spec", "RSL6d")]
+    public class DeltaLastMessageSpecs : AblyRealtimeSpecs
+    {
+        [Fact]
+        [Trait("spec", "RSL6d2")]
+        public async Task WithStringMessage_ShouldKeepItWithoutProcessing()
+        {
+            var (realtime, c) = await GetClientAndChannel();
+            RealtimeChannel channel = (RealtimeChannel)c;
+            channel.SetChannelState(ChannelState.Attached);
+
+            var successfulProtocolMessage = new ProtocolMessage(ProtocolMessage.MessageAction.Message)
+            {
+                Channel = channel.Name,
+                Messages = new[] { new Message { Data = "test", Encoding = "custom" }, },
+            };
+            realtime.ExecuteCommand(ProcessMessageCommand.Create(successfulProtocolMessage));
+            await realtime.ProcessCommands();
+
+            var previousPayload = channel.MessageDecodingContext.PreviousPayload;
+            previousPayload.Encoding.Should().Be("custom");
+            previousPayload.StringData.Should().Be("test");
+            previousPayload.ByteData.Should().BeNull();
+        }
+
+        [Fact]
+        [Trait("spec", "RSL6d2")]
+        public async Task WithBinaryMessage_ShouldKeepItPreviousPayloadInBinaryForm()
+        {
+            var (realtime, c) = await GetClientAndChannel();
+            RealtimeChannel channel = (RealtimeChannel)c;
+            channel.SetChannelState(ChannelState.Attached);
+
+            var binaryData = new byte[] { 112, 100, 111 };
+            var successfulProtocolMessage = new ProtocolMessage(ProtocolMessage.MessageAction.Message)
+            {
+                Channel = channel.Name,
+                Messages = new[] { new Message { Data = binaryData }, },
+            };
+            realtime.ExecuteCommand(ProcessMessageCommand.Create(successfulProtocolMessage));
+            await realtime.ProcessCommands();
+
+            var previousPayload = channel.MessageDecodingContext.PreviousPayload;
+            previousPayload.StringData.Should().BeNull();
+            previousPayload.ByteData.Should().Equal(binaryData);
+        }
+
+        [Fact]
+        [Trait("spec", "RSL6d1")]
+        public async Task WithBase64Message_ShouldKeepBase64DecodedPayload()
+        {
+            var (realtime, c) = await GetClientAndChannel();
+            RealtimeChannel channel = (RealtimeChannel)c;
+            channel.SetChannelState(ChannelState.Attached);
+
+            var testString = "Testing, testing, testing";
+            var successfulProtocolMessage = new ProtocolMessage(ProtocolMessage.MessageAction.Message)
+            {
+                Channel = channel.Name,
+                Messages = new[] { new Message { Data = testString.ToBase64(), Encoding = "base64" }, },
+            };
+            realtime.ExecuteCommand(ProcessMessageCommand.Create(successfulProtocolMessage));
+
+            await realtime.ProcessCommands();
+
+            var previousPayload = channel.MessageDecodingContext.PreviousPayload;
+            previousPayload.StringData.Should().BeNull();
+            previousPayload.Encoding.Should().BeEmpty();
+            previousPayload.ByteData.Should().Equal(testString.GetBytes());
+        }
+
+        public DeltaLastMessageSpecs(ITestOutputHelper output)
+        : base(output)
+        {
+        }
+    }
+}
