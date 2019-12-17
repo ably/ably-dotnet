8b74f6b5d1068634f2f49c32a88a626cf5cddf97
Martin Georgiev <marto83@gmail.com>
2019-12-17 22:16:20 +0000

    Remove unused Util functions

--- src/IO.Ably.Tests.Shared/TaskUtilsTests.cs
+++ src/IO.Ably.Tests.Shared/TaskUtilsTests.cs
@@ -32,22 +32,5 @@ namespace IO.Ably.Tests
             result.Should().BeTrue();
         }
 
-        [Fact(Skip="This kills the test runner. ONLY run manually")]
-        public async Task FailingTaskWrappingAsyncWillNotHaveExceptionHandled()
-        {
-            /*
-             *  This test demonstrates that wrapping a method that throws an Exception will cause the handler
-             *  to not be assigned at the continuation for the inner task and as such the Exception will not be handled.
-             *  DO NOT USE IN PRODUCTION CODE.
-             */
-            var tsc = new TaskCompletionAwaiter(500);
-            TaskUtils.RunInBackground(async () => await MockBagroundService.FailingTask(), exception =>
-            {
-                tsc.SetCompleted();
-            });
-
-            var result = await tsc.Task;
-            result.Should().BeFalse();
-        }
     }
 }
