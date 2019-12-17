ff5cc73439d4ff7f0a748b1ec3a073466d6892e2
Martin Georgiev <marto83@gmail.com>
2019-12-17 22:16:20 +0000

    Remove unused Util functions

--- src/IO.Ably.Shared/Utils/TaskUtils.cs
+++ src/IO.Ably.Shared/Utils/TaskUtils.cs
@@ -51,35 +51,6 @@ namespace IO.Ably
             };
 
         /// <summary>
-        /// Run a synchronous method in a fire-and-forget manner.
-        /// An optional handler can be provided to process exceptions, if they occur.
-        /// </summary>
-        internal static void RunInBackground(Action action, Action<Exception> handler = null)
-        {
-            if (action == null)
-            {
-                throw new ArgumentNullException(nameof(action));
-            }
-
-            var task = Task.Run(action);
-
-            if (handler == null)
-            {
-                task.ContinueWith(
-                    DefaultErrorContinuation,
-                    TaskContinuationOptions.ExecuteSynchronously |
-                    TaskContinuationOptions.OnlyOnFaulted);
-            }
-            else
-            {
-                task.ContinueWith(
-                    t => handler(t.Exception.GetBaseException()),
-                    TaskContinuationOptions.ExecuteSynchronously |
-                    TaskContinuationOptions.OnlyOnFaulted);
-            }
-        }
-
-        /// <summary>
         /// Run a <see cref="Task"/> in a fire-and-forget manner.
         /// An optional handler can be provided to process exceptions, if they occur.
         /// </summary>
