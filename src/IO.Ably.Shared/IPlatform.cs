19dd8d8a4e2f1c6ebe8ee609e191c69f50f4f5db
Martin Georgiev <marto83@gmail.com>
2020-01-17 10:23:57 +0000

    Add comments to the IPlatform interface.

--- src/IO.Ably.Shared/IPlatform.cs
+++ src/IO.Ably.Shared/IPlatform.cs
@@ -6,12 +6,27 @@ namespace IO.Ably
         "StyleCop.CSharp.DocumentationRules",
         "SA1600:Elements should be documented",
         Justification = "Internal interface")]
+
+    /// <summary>
+    /// This interface is implemented for each platform .NETFramework, NetStandard,
+    /// iOS, Android and UWP. The library dynamically creates an instance of Platform in
+    /// IoC.cs. It lets us deal with the differences in the various platforms.
+    /// </summary>
     internal interface IPlatform
     {
         string PlatformId { get; }
 
         ITransportFactory TransportFactory { get; }
 
+        /// <summary>
+        /// This method when implemented in each Platform class includes logic to subscribe to
+        /// NetworkStatus changes from the operating system. It is then exposed through
+        /// IoC.RegisterOsNetworkStateChanged to the rest of the library and should be called
+        /// when the Realtime library is initialised only if the ClientOption `AutomaticNetworkStateMonitoring`
+        /// is set to true.
+        /// The implementation will only allow one registration to operating system network state events even
+        /// thought this method can be called multiple times.
+        /// </summary>
         void RegisterOsNetworkStateChanged();
     }
 }
