fe525df74fde67822d228c96723e8c072451420f
Martin Georgiev <marto83@gmail.com>
2020-02-07 16:09:57 +0000

    Update all spec ids to new ones

--- src/IO.Ably.Tests.Shared/Infrastructure/ConditionalAwaiter.cs
+++ src/IO.Ably.Tests.Shared/Infrastructure/ConditionalAwaiter.cs
@@ -10,13 +10,15 @@ namespace IO.Ably.Tests.DotNetCore20.Infrastructure
     public class ConditionalAwaiter
     {
         private readonly Func<bool> _condition;
+        private readonly Func<string> _getError;
         private Timer _timer = new Timer();
         private TaskCompletionSource<bool> _completionSource;
         private int _tickCount = 0;
 
-        public ConditionalAwaiter(Func<bool> condition)
+        public ConditionalAwaiter(Func<bool> condition, Func<string> getError = null)
         {
             _condition = condition;
+            _getError = getError;
             _timer.Enabled = true;
             _timer.Interval = 100;
             _timer.Elapsed += TimerOnElapsed;
@@ -28,7 +30,7 @@ namespace IO.Ably.Tests.DotNetCore20.Infrastructure
             Interlocked.Increment(ref _tickCount);
             if (_tickCount > 100)
             {
-                _completionSource.SetException(new Exception("10 seconds elapsed. Giving up"));
+                _completionSource.SetException(new Exception("10 seconds elapsed. Giving up. Error: " + _getError?.Invoke()));
             }
 
             if (_condition() && _completionSource.Task.IsCompleted == false)
