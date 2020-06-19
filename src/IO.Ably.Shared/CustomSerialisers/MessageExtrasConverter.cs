7953e72cf8a9df3e9b989d05299c0600e0ce5ea1
Martin Georgiev <marto83@gmail.com>
2020-06-19 18:32:50 +0100

    Add custom json converter for MessageExtras

---
+++ src/IO.Ably.Shared/CustomSerialisers/MessageExtrasConverter.cs
@@ -0,0 +1,34 @@
+using System;
+using IO.Ably.Types;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace IO.Ably.CustomSerialisers
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable SA1600 // Elements should be documented
+    public class MessageExtrasConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is MessageExtras messageExtras)
+            {
+                // TODO: Investigate if there is a better way to do that.
+                writer.WriteRawValue(messageExtras.ToJson().ToString());
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var extrasToken = JToken.Load(reader);
+            return new MessageExtras(extrasToken);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(MessageExtras);
+        }
+    }
+#pragma warning restore SA1600 // Elements should be documented
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
