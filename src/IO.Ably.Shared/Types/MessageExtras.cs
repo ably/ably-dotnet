48f32098a705cc55944e9bc0b62d58703939b012
Martin Georgiev <marto83@gmail.com>
2020-06-19 18:32:14 +0100

    Update MessageExtras object to take a custom json

--- src/IO.Ably.Shared/Types/MessageExtras.cs
+++ src/IO.Ably.Shared/Types/MessageExtras.cs
@@ -1,3 +1,6 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace IO.Ably.Types
 {
     /// <summary>
@@ -5,10 +8,52 @@ namespace IO.Ably.Types
     /// </summary>
     public class MessageExtras
     {
+        private const string DeltaProperty = "delta";
+
+        [JsonIgnore]
+        private JToken Data { get; set; }
+
         /// <summary>
         /// Delta extras is part of the Ably delta's functionality.
         /// </summary>
         public DeltaExtras Delta { get; set; }
+
+        /// <summary>
+        /// Messages extras is a flexible object that may other properties that are not exposed by the strongly typed implementation.
+        /// </summary>
+        /// <param name="data">the json object passed to Message extras.</param>
+        public MessageExtras(JToken data = null)
+        {
+            Data = data;
+            ParseData();
+        }
+
+        private void ParseData()
+        {
+            if (Data != null && Data is JObject dataObject)
+            {
+                var deltaProp = dataObject[DeltaProperty];
+                if (deltaProp != null && deltaProp is JObject deltaObject)
+                {
+                    Delta = deltaObject.ToObject<DeltaExtras>();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the inner message extras json object including any changes made to DeltaExtras.
+        /// </summary>
+        /// <returns>returns the inner json.</returns>
+        public JToken ToJson()
+        {
+            var result = Data?.DeepClone();
+            if (result != null && Delta != null)
+            {
+                result[DeltaProperty] = JObject.FromObject(Delta);
+            }
+
+            return result;
+        }
     }
 
     /// <summary>
