bfbcf18073536ebdfc1da0f169e2dc66968c8d86
sacOO7 <sachinshinde7676@gmail.com>
2020-08-21 01:25:27 +0530

    Added new LogHandler property to ClientOptions, delegated old LogHander to new implementation for LogHandler, added unit tests for the same

--- examples/RealtimeChat/RealtimeChat/MainWindow.xaml.cs
+++ examples/RealtimeChat/RealtimeChat/MainWindow.xaml.cs
@@ -67,7 +67,7 @@ namespace RealtimeChat
                 AutoConnect = false,
                 ClientId = clientId,
                 LogLevel = LogLevel.Debug,
-                LogHander = new CustomLoggerSink(message => Context.Post(state => logBox.Items.Add(message), null))
+                LogHandler = new CustomLoggerSink(message => Context.Post(state => logBox.Items.Add(message), null))
             };
             this.client = new AblyRealtime(options);
             this.client.Connection.ConnectionStateChanged += this.connection_ConnectionStateChanged;
---
+++ src/IO.Ably.Tests.Shared/ClientOptionsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IO.Ably.AcceptanceTests;
+using Microsoft.Build.Logging;
+using Xunit;
+
+namespace IO.Ably.Tests.Shared
+{
+    public class ClientOptionsTests
+    {
+        [Fact]
+        [Obsolete]
+        public void Options_WithLogHanderReturns_LogHandlerWithSameLoggerSink()
+        {
+            var testLoggerSink = new TestLoggerSink();
+            var options = new ClientOptions()
+            {
+                LogHander = testLoggerSink
+            };
+
+            Assert.Same(options.LogHander, testLoggerSink);
+            Assert.Same(options.LogHandler, testLoggerSink);
+
+            var debugLoggerSink = new DefaultLoggerSink();
+            options.LogHander = debugLoggerSink;
+
+            Assert.Same(options.LogHander, debugLoggerSink);
+            Assert.Same(options.LogHandler, debugLoggerSink);
+        }
+
+        [Fact]
+        [Obsolete]
+        public void Options_WithLogHandlerReturns_LogHanderWithSameLoggerSink()
+        {
+            var testLoggerSink = new TestLoggerSink();
+            var options = new ClientOptions()
+            {
+                LogHandler = testLoggerSink
+            };
+
+            Assert.Same(options.LogHandler, testLoggerSink);
+            Assert.Same(options.LogHander, testLoggerSink);
+
+            var debugLoggerSink = new DefaultLoggerSink();
+            options.LogHandler = debugLoggerSink;
+
+            Assert.Same(options.LogHandler, debugLoggerSink);
+            Assert.Same(options.LogHander, debugLoggerSink);
+        }
+    }
+}
