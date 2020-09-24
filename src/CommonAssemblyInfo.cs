b5472037643bd7a4df32326005f12f479b5dfb38
Martin Georgiev <marto83@gmail.com>
2020-09-24 15:59:44 +0100

    Dump version number to 1.2.2

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Reflection;
 [assembly: AssemblyCompany("Ably Realtime")]
 [assembly: AssemblyDescription("Client for ably.io realtime service")]
 [assembly: AssemblyProduct("Ably .Net Library")]
-[assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1")]
+[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyFileVersion("1.2.2")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const System.String AssemblyCompany = "Ably Realtime";
         internal const System.String AssemblyDescription = "Client for ably.io realtime service";
         internal const System.String AssemblyProduct = "Ably .Net Library";
-        internal const System.String AssemblyVersion = "1.2.1";
-        internal const System.String AssemblyFileVersion = "1.2.1";
+        internal const System.String AssemblyVersion = "1.2.2";
+        internal const System.String AssemblyFileVersion = "1.2.2";
     }
 }
