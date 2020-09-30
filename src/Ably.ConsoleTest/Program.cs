aad585f8f5ef242d6a8d8113f948447f21d32fdd
Martin Georgiev <marto83@gmail.com>
2020-09-30 10:50:50 +0100

    Add test messages of varying sizes

--- src/Ably.ConsoleTest/Program.cs
+++ src/Ably.ConsoleTest/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace IO.Ably.ConsoleTest
 {
@@ -16,20 +20,35 @@ namespace IO.Ably.ConsoleTest
         {
             IO.Ably.DefaultLogger.LoggerSink = new MyLogger();
             DefaultLogger.LogLevel = LogLevel.Warning;
+            var data = File.ReadAllText("test16k.json");
+            var message = JToken.Parse(data);
             try
             {
                 //Rest.Test().Wait();
                 var client = Realtime.Test();
-                client.Connect(); 
+                client.Connect();
                 var channel = client.Channels.Get("Martin");
                 await channel.AttachAsync();
                 await channel.Presence.EnterAsync();
-                DateTime start = DateTime.Now;
+                client.Connection.On(changes =>
+                {
+                    ConsoleColor.Magenta.WriteLine($"Connection: {changes.Current} from {changes.Previous}. Error: {(changes.HasError ? changes.Reason.Message : "")}");
+                });
+                int count = 0;
+                int succeeded = 0;
                 while (true)
                 {
-                    channel.Publish(new Random().Next(1000000000, 1000000000).ToString(), new Random().Next(1000000000, 1000000000).ToString(), (s, error) => Console.WriteLine("Message sent: " + s));
-                    await Task.Delay(1000);
-                    Console.WriteLine("Connected time: " + (DateTime.Now - start).TotalSeconds + " seconds.");
+                    channel.Publish(new Random().Next(1000000000, 1000000000).ToString(), message, (s, error) =>
+                    {
+                        if (s == false)
+                            ConsoleColor.Red.WriteLine("Error sending message. Error: " + error);
+                        else
+                            succeeded++;
+                    });
+                    await Task.Delay(100);
+                    count++;
+                    if(count % 10 == 0)
+                        ConsoleColor.Green.WriteLine("Messages sent: " + count + ". Succeeded: " + succeeded);
                 }
 
                 Console.ReadLine();
