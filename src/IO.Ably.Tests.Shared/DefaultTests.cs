4c8824abae6db3bf129f6da8e537e98ac45d6a27
sacOO7 <sachinshinde7676@gmail.com>
2020-10-20 20:11:22 +0530

    Added unit test for default env. fallback hosts

---
+++ src/IO.Ably.Tests.Shared/DefaultTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using IO.Ably.AcceptanceTests;
+using Xunit;
+
+namespace IO.Ably.Tests.Shared
+{
+    public class DefaultTests
+    {
+        [Fact]
+        public void Defaults_WithEnvironment_ReturnsEnvironmentFallbackHosts()
+        {
+            var expectedFallBackHosts = new[]
+            {
+                "sandbox-a-fallback.ably-realtime.com",
+                "sandbox-b-fallback.ably-realtime.com",
+                "sandbox-c-fallback.ably-realtime.com",
+                "sandbox-d-fallback.ably-realtime.com",
+                "sandbox-e-fallback.ably-realtime.com"
+            };
+            var fallbackHosts = Defaults.GetEnvironmentFallbackHosts("sandbox");
+            Assert.Equal(expectedFallBackHosts, fallbackHosts);
+        }
+    }
+}
