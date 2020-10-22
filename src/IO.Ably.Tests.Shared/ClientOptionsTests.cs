9e0cf0df34a32029caf21623121a3ee9f08fe6fd
sacOO7 <sachinshinde7676@gmail.com>
2020-10-23 01:42:12 +0530

    Added fallback tests for clientOptions

--- src/IO.Ably.Tests.Shared/ClientOptionsTests.cs
+++ src/IO.Ably.Tests.Shared/ClientOptionsTests.cs
@@ -48,5 +48,178 @@ namespace IO.Ably.Tests.Shared
             Assert.Same(options.LogHandler, debugLoggerSink);
             Assert.Same(options.LogHander, debugLoggerSink);
         }
+
+        [Fact]
+        public void DefaultOptions()
+        {
+            var options = new ClientOptions();
+            Assert.Equal("realtime.ably.io", options.RealtimeHost);
+            Assert.Equal("rest.ably.io", options.RestHost);
+            Assert.Equal(80, options.Port);
+            Assert.Equal(443, options.TlsPort);
+            Assert.Equal(Defaults.FallbackHosts, options.FallbackHosts);
+            Assert.True(options.Tls);
+        }
+
+        [Fact]
+        public void Options_WithProductionEnvironment()
+        {
+            var options = new ClientOptions()
+            {
+                Environment = "production"
+            };
+            Assert.Equal("realtime.ably.io", options.RealtimeHost);
+            Assert.Equal("rest.ably.io", options.RestHost);
+            Assert.Equal(80, options.Port);
+            Assert.Equal(443, options.TlsPort);
+            Assert.Equal(Defaults.FallbackHosts, options.FallbackHosts);
+            Assert.True(options.Tls);
+        }
+
+        [Fact]
+        public void Options_WithCustomEnvironment()
+        {
+            var options = new ClientOptions()
+            {
+                Environment = "sandbox"
+            };
+            Assert.Equal("sandbox-realtime.ably.io", options.RealtimeHost);
+            Assert.Equal("sandbox-rest.ably.io", options.RestHost);
+            Assert.Equal(80, options.Port);
+            Assert.Equal(443, options.TlsPort);
+            Assert.Equal(Defaults.GetEnvironmentFallbackHosts("sandbox"), options.FallbackHosts);
+            Assert.True(options.Tls);
+        }
+
+        [Fact]
+        [Obsolete]
+        public void Options_WithCustomEnvironment_And_DefaultFallbacks()
+        {
+            var options = new ClientOptions()
+            {
+                Environment = "sandbox",
+                FallbackHostsUseDefault = true
+            };
+
+            Assert.Equal("sandbox-realtime.ably.io", options.RealtimeHost);
+            Assert.Equal("sandbox-rest.ably.io", options.RestHost);
+            Assert.Equal(80, options.Port);
+            Assert.Equal(443, options.TlsPort);
+            Assert.Equal(Defaults.GetEnvironmentFallbackHosts("sandbox"), options.FallbackHosts);
+            Assert.True(options.Tls);
+        }
+
+        [Fact]
+        public void Options_WithCustomEnvironment_And_NonDefaultPorts()
+        {
+            var options = new ClientOptions()
+            {
+                Environment = "local",
+                Port = 8080,
+                TlsPort = 8081
+            };
+
+            Assert.Equal("local-realtime.ably.io", options.RealtimeHost);
+            Assert.Equal("local-rest.ably.io", options.RestHost);
+            Assert.Equal(8080, options.Port);
+            Assert.Equal(8081, options.TlsPort);
+            Assert.Empty(options.FallbackHosts);
+            Assert.True(options.Tls);
+        }
+
+        [Fact]
+        public void Options_WithCustomHost()
+        {
+            var options = new ClientOptions()
+            {
+                RestHost = "test.org"
+            };
+
+            Assert.Equal("test.org", options.RestHost);
+            Assert.Equal("test.org", options.RealtimeHost);
+            Assert.Equal(80, options.Port);
+            Assert.Equal(443, options.TlsPort);
+            Assert.Empty(options.FallbackHosts);
+            Assert.True(options.Tls);
+        }
+
+        [Fact]
+        public void Options_WithCustomRestHost_And_RealtimeHost()
+        {
+            var options = new ClientOptions()
+            {
+                RestHost = "test.org",
+                RealtimeHost = "ws.test.org"
+            };
+
+            Assert.Equal("test.org", options.RestHost);
+            Assert.Equal("ws.test.org", options.RealtimeHost);
+            Assert.Equal(80, options.Port);
+            Assert.Equal(443, options.TlsPort);
+            Assert.Empty(options.FallbackHosts);
+            Assert.True(options.Tls);
+        }
+
+        [Fact]
+        [Obsolete]
+        public void Options_WithCustomRestHost_And_RealtimeHost_And_DefaultFallbacks()
+        {
+            var options = new ClientOptions()
+            {
+                RestHost = "test.org",
+                RealtimeHost = "ws.test.org",
+                FallbackHostsUseDefault = true
+            };
+
+            Assert.Equal("test.org", options.RestHost);
+            Assert.Equal("ws.test.org", options.RealtimeHost);
+            Assert.Equal(80, options.Port);
+            Assert.Equal(443, options.TlsPort);
+            Assert.Equal(Defaults.FallbackHosts, options.FallbackHosts);
+            Assert.True(options.Tls);
+        }
+
+        [Fact]
+        [Obsolete]
+        public void Options_With_FallbackHosts_And_FallbackHostsUseDefault()
+        {
+            var options = new ClientOptions()
+            {
+                FallbackHosts = new[] { "a.example.com", "b.example.com" },
+                FallbackHostsUseDefault = true
+            };
+
+            var ex = Assert.Throws<AblyException>(() => { var unused = options.FallbackHosts; });
+            Assert.Equal("fallbackHosts and fallbackHostsUseDefault cannot both be set", ex.ErrorInfo.Message);
+        }
+
+        [Fact]
+        [Obsolete]
+        public void Options_With_And_FallbackHostsUseDefault_And_Port_Or_TlsPort()
+        {
+            var options = new ClientOptions()
+            {
+                FallbackHostsUseDefault = true,
+                Port = 8080
+            };
+
+            var ex = Assert.Throws<AblyException>(() =>
+            {
+                var unused = options.FallbackHosts;
+            });
+            Assert.Equal("fallbackHostsUseDefault cannot be set when port or tlsPort are set", ex.ErrorInfo.Message);
+
+            options = new ClientOptions()
+            {
+                FallbackHostsUseDefault = true,
+                TlsPort = 8081
+            };
+
+            ex = Assert.Throws<AblyException>(() =>
+            {
+                var unused = options.FallbackHosts;
+            });
+            Assert.Equal("fallbackHostsUseDefault cannot be set when port or tlsPort are set", ex.ErrorInfo.Message);
+        }
     }
 }
