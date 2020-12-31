bc360ec4ab462dca02c584e43a882bcb1fe41576
sacOO7 <sachinshinde7676@gmail.com>
2021-01-01 03:31:18 +0530

    Added custom message data converter with NullValueHandling enabled

---
+++ src/IO.Ably.Shared/CustomSerialisers/MessageDataConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace IO.Ably.Shared.CustomSerialisers
+{
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable SA1600 // Elements should be documented
+    public class MessageDataConverter : JsonConverter
+    {
+        /// <inheritdoc/>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            serializer.NullValueHandling = NullValueHandling.Include;
+            serializer.Serialize(writer, value);
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+        }
+
+        /// <inheritdoc/>
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return serializer.Deserialize(reader, objectType);
+        }
+
+        /// <inheritdoc/>
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+    }
+}
