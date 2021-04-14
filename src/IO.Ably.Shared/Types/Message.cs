30cd20098b964d9271a8841348013ac9619ec38b
Jason Smith <jason@jasonmsmith.biz>
2021-04-14 11:56:55 +0300

    Add ConnectionKey property to messages

--- src/IO.Ably.Shared/Types/Message.cs
+++ src/IO.Ably.Shared/Types/Message.cs
@@ -51,6 +51,10 @@ namespace IO.Ably
         [JsonProperty("connectionId")]
         public string ConnectionId { get; set; }
 
+        /// <summary>The connection key of the publisher of the message. Used for impersonation.</summary>
+        [JsonProperty("connectionKey")]
+        public string ConnectionKey { get; set; }
+
         /// <summary>The event name, if available.</summary>
         [JsonProperty("name")]
         public string Name { get; set; }
