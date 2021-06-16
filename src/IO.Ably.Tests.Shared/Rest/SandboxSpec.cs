8b07226aa57be12a04c6105c5d3d0eb1ae8378ea
Tom Kirby-Green <tomkirbygreen@gmail.com>
2021-06-16 12:13:44 +0100

    Remove duplicated code.

--- src/IO.Ably.Tests.Shared/Rest/SandboxSpec.cs
+++ src/IO.Ably.Tests.Shared/Rest/SandboxSpec.cs
@@ -194,17 +194,5 @@ namespace IO.Ably.Tests
 
             ResetEvent?.Dispose();
         }
-
-        public IDisposable EnableDebugLogging()
-        {
-            Logger.LoggerSink = new SandboxSpecs.OutputLoggerSink(Output);
-            Logger.LogLevel = LogLevel.Debug;
-
-            return new ActionOnDispose(() =>
-            {
-                Logger.LoggerSink = new DefaultLoggerSink();
-                Logger.LogLevel = LogLevel.Warning;
-            });
-        }
     }
 }
