e5b80ac626028e7d5d68697e14631a9683b492bf
Martin Georgiev <martin@stadion.io>
2021-06-21 18:09:34 +0100

    Add PushRest and PushAdmin classes as placeholders for Push Rest Apis

---
+++ src/IO.Ably.Shared/Push/PushAdmin.cs
@@ -0,0 +1,17 @@
+﻿namespace IO.Ably.Push
+{
+    /// <summary>
+    /// Push Admin APIs.
+    /// </summary>
+    public class PushAdmin
+    {
+        private readonly AblyRest _restClient;
+        private readonly ILogger _logger;
+
+        internal PushAdmin(AblyRest restClient, ILogger logger)
+        {
+            _restClient = restClient;
+            _logger = logger;
+        }
+    }
+}
---
+++ src/IO.Ably.Shared/Push/PushRest.cs
@@ -0,0 +1,23 @@
+﻿namespace IO.Ably.Push
+{
+    /// <summary>
+    /// Push Apis for Rest and Realtime clients.
+    /// </summary>
+    public class PushRest
+    {
+        private readonly AblyRest _rest;
+        private readonly ILogger _logger;
+
+        internal PushRest(AblyRest rest, ILogger logger)
+        {
+            _rest = rest;
+            _logger = logger;
+            Admin = new PushAdmin(rest, logger);
+        }
+
+        /// <summary>
+        /// Admin APIs for Push notifications.
+        /// </summary>
+        public PushAdmin Admin { get; }
+    }
+}
