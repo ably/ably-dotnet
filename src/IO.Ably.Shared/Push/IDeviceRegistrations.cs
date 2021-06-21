7fadee0a303a9027844dee73c55ecbc588d87597
Martin Georgiev <martin@stadion.io>
2021-06-21 23:16:06 +0100

    Add DeviceRegistrations and PushChannelSubscriptions interfaces for PushAdmin.

---
+++ src/IO.Ably.Shared/Push/IDeviceRegistrations.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+
+namespace IO.Ably.Push
+{
+    /// <summary>
+    /// Device Registrations APIs. For more information visit the Ably Rest Documentation: https://ably.com/documentation/rest-api#post-device-registration.
+    /// </summary>
+    public interface IDeviceRegistrations
+    {
+        /// <summary>
+        /// Update a device registration.
+        /// </summary>
+        /// <param name="details">Device to be updated. Only `clientId`, `metadata` and `Push.Recipient` can be updates. The rest of the values must match what the server contains.
+        /// RestApi: https://ably.com/documentation/rest-api#update-device-registration.</param>
+        /// <returns>Updated Device Registration.</returns>
+        Task<DeviceDetails> SaveAsync(DeviceDetails details); // The Rest API only allows clientId, metadata and push.recipient to be updates FYI: https://ably.com/documentation/rest-api#update-device-registration
+
+        /// <summary>
+        /// Obtain the details for a device registered for receiving push registrations.
+        /// RestApi: https://ably.com/documentation/rest-api#get-device-registration.
+        /// </summary>
+        /// <param name="deviceId">Id of the device.</param>
+        /// <returns>Returns a DeviceDetails class if the device is found or `null` if not.</returns>
+        Task<DeviceDetails> GetAsync(string deviceId);
+
+        /// <summary>
+        /// Obtain the details for devices registered for receiving push registrations.
+        /// RestApi: https://ably.com/documentation/rest-api#list-device-registrations.
+        /// </summary>
+        /// <param name="clientId">Optional filter by clientId. DeviceId and ClientId cannot be combined.</param>
+        /// <param name="deviceId">Optional filter by deviceId. DeviceId and ClientId cannot be combined.</param>
+        /// <param name="limit">Number of restults returned. Max allowed value: 1000.</param>
+        /// <returns>A paginated list of DeviceDetails.</returns>
+        Task<PaginatedResult<DeviceDetails>> List(string clientId = null, string deviceId = null, int? limit = null);
+
+        /// <summary>
+        /// Removes a registered device.
+        /// RestAPI: https://ably.com/documentation/rest-api#delete-device-registration.
+        /// </summary>
+        /// <param name="details">DeviceDetails to be removed.</param>
+        /// <returns>Task.</returns>
+        Task RemoveAsync(DeviceDetails details);
+
+        /// <summary>
+        /// Removes a registered device.
+        /// RestAPI: https://ably.com/documentation/rest-api#delete-device-registration.
+        /// </summary>
+        /// <param name="deviceId">The deviceId of the device to be removed.</param>
+        /// <returns>Task.</returns>
+        Task RemoveAsync(string deviceId);
+
+        // TODO: Discuss with Tom whether to add more helper methods to cover the full Rest Api.
+    }
+}
---
+++ src/IO.Ably.Shared/Push/IPushChannelSubscriptions.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+
+namespace IO.Ably.Push
+{
+    /// <summary>
+    /// Apis for managing ChannelSubscriptions.
+    /// </summary>
+    public interface IPushChannelSubscriptions
+    {
+        /// <summary>
+        /// Subscribe either a single device or all devices associated with a client ID to receive push notifications from messages sent to a channel.
+        /// RestApi: https://ably.com/documentation/rest-api#post-channel-subscription.
+        /// </summary>
+        /// <param name="subscription">Channel subscription object.</param>
+        /// <returns>Return an updated Channel subscription object.</returns>
+        Task<ChannelSubscription> SaveAsync(ChannelSubscription subscription);
+
+        /// <summary>
+        /// Get a list of push notification subscriptions to channels.
+        /// RestApi: https://ably.com/documentation/rest-api#list-channel-subscriptions.
+        /// </summary>
+        /// <param name="channel">Filter by channel name.</param>
+        /// <param name="clientId">Filter to restrict to subscriptions associated with that clientId. Cannot be used together with deviceId.</param>
+        /// <param name="deviceId">Filter to restrict to subscriptions associated with that deviceId. Cannot be used together with clientId.</param>
+        /// <param name="limit">Number of returns to return. Max limit is 1000.</param>
+        /// <returns>Returns a paginated list of ChannelSubscriptions.</returns>
+        Task<PaginatedResult<ChannelSubscription>> ListAsync(string channel, string clientId = null, string deviceId = null, int? limit = null); // TODO: Add request object
+
+        /// <summary>
+        /// Stop receiving push notifications when push messages are published on the specified channels.
+        /// Please note that this operation is done asynchronously so immediate requests subsequent to this delete request may briefly still return the subscription.
+        /// RestApi: https://ably.com/documentation/rest-api#delete-channel-subscription.
+        /// </summary>
+        /// <param name="subscription">Channel Subscription object to unsubscribe.</param>
+        /// <returns>Task.</returns>
+        Task RemoveAsync(ChannelSubscription subscription); // TODO: Add request object and allow other params
+
+        /// <summary>
+        /// List all channels with at least one subscribed device.
+        /// RestApi: https://ably.com/documentation/rest-api#list-channels.
+        /// </summary>
+        /// <returns>Paginated list of channel names.</returns>
+        Task<PaginatedResult<string>> ListChannelsAsync(); // TODO: Create a return type that is not string. Note: Java implementation has possible deviceId parameter but the REST documentation doesn't include it.
+    }
+}
