fa7f9b1d1601b8e9e954beb734bbfb3c006ee736
Martin Georgiev <marto83@gmail.com>
2021-06-22 12:53:22 +0100

    Add IMobileDevice interface and implement CallDeactivatedCallback

--- src/IO.Ably.Shared/Push/ActivationStateMachine.cs
+++ src/IO.Ably.Shared/Push/ActivationStateMachine.cs
@@ -5,21 +5,23 @@ namespace IO.Ably.Push
     internal partial class ActivationStateMachine
     {
         private readonly AblyRest _restClient;
+        private readonly IMobileDevice _mobileDevice;
         private readonly ILogger _logger;
 
         private Queue<Event> PendingEvents { get; set; } = new Queue<Event>();
 
-        internal ActivationStateMachine(AblyRest restClient, ILogger logger)
+        internal ActivationStateMachine(AblyRest restClient, IMobileDevice mobileDevice, ILogger logger)
         {
             _restClient = restClient;
+            _mobileDevice = mobileDevice;
             _logger = logger;
         }
 
         public LocalDevice LocalDevice { get; set; }
 
-        private void CallDeactivatedCallback(object o)
+        private void CallDeactivatedCallback(ErrorInfo reason)
         {
-            throw new System.NotImplementedException();
+            SendErrorIntent("PUSH_DEACTIVATE", reason); // TODO: Put intent names in consts
         }
 
         private void ValidateRegistration()
@@ -41,9 +43,23 @@ namespace IO.Ably.Push
         {
             throw new System.NotImplementedException();
         }
+
+        private void SendErrorIntent(string name, ErrorInfo errorInfo)
+        {
+            var properties = new Dictionary<string, object>();
+            properties.Add("hasError", errorInfo != null);
+            if (errorInfo != null) {
+                properties.Add("error.message", errorInfo.Message);
+                properties.Add("error.statusCode", (int?)errorInfo.StatusCode);
+                properties.Add("error.code", errorInfo.Code);
+            }
+
+            _mobileDevice.SendIntent(name, properties);
+        }
+
+        private void SendIntent(string name)
+        {
+            _mobileDevice.SendIntent(name, new Dictionary<string, object>());
+        }
     }
-
-
-
-
 }
---
+++ src/IO.Ably.Shared/Push/IMobileDevice.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace IO.Ably.Push
+{
+    /// <summary>
+    /// Interface for communicating with a mobile device supporting pushing notifications
+    /// </summary>
+    public interface IMobileDevice
+    {
+        /// <summary>
+        /// Trigger Intent(Android) or ... (Apple)
+        /// </summary>
+        /// <param name="name">name of the intent. It will be prepended with io.ably.broadcast..</param>
+        /// <param name="extraParameters">extra parameters set for the intent.</param>
+        void SendIntent(string name, Dictionary<string, object> extraParameters);
+    }
+}
