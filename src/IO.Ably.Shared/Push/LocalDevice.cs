7cc0439d156a708beafd1c50f3278cde72b01683
Martin Georgiev <marto83@gmail.com>
2021-06-22 14:24:32 +0100

    Add Factory method to LocalDevice to create a new Device

--- src/IO.Ably.Shared/Push/LocalDevice.cs
+++ src/IO.Ably.Shared/Push/LocalDevice.cs
@@ -1,4 +1,7 @@
-﻿namespace IO.Ably.Push
+﻿using System;
+using IO.Ably.Encryption;
+
+namespace IO.Ably.Push
 {
     /// <summary>
     /// LocalDevice represents the current state of the device in respect of it being a target for push notifications.
@@ -25,6 +28,21 @@
             }
 
             return null;
+        }
+
+        /// <summary>
+        /// Create a new instance of localDevice with a random Id and secret.
+        /// </summary>
+        /// <param name="clientId">The clientId which is set on the device. Can be null.</param>
+        /// <returns>Instance of LocalDevice</returns>
+        public static LocalDevice Create(string clientId)
+        {
+            return new LocalDevice
+            {
+                Id = Guid.NewGuid().ToString("D"),
+                DeviceSecret = Crypto.GenerateSecret(),
+                ClientId = clientId
+            };
         }
     }
 }
