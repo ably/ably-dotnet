ab1e5178e5e2fad3091cff0f0823940ec402522c
Martin Georgiev <marto83@gmail.com>
2021-06-22 11:28:24 +0100

    Add NotActivated State and Transition logic

---
+++ src/IO.Ably.Shared/Push/ActivationStateMachine.States.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace IO.Ably.Push
+{
+    internal partial class ActivationStateMachine
+    {
+        public abstract class State
+        {
+            protected ActivationStateMachine Machine { get; }
+
+            public State(ActivationStateMachine machine)
+            {
+                Machine = machine;
+            }
+
+            public abstract bool Persist { get; }
+
+            public abstract Task<State> Transition(ActivationStateMachine.Event @event);
+        }
+
+        public sealed class NotActivated : State
+        {
+            public override bool Persist => true;
+
+            public NotActivated(ActivationStateMachine machine)
+                : base(machine)
+            {
+            }
+
+            public override async Task<State> Transition(Event @event)
+            {
+                switch (@event)
+                {
+                    case CalledDeactivate _:
+                        Machine.CallDeactivatedCallback(null);
+                        return this;
+                    case CalledActivate _:
+                        // TODO: Logging
+                        var device = Machine.LocalDevice;
+
+                        if (device.IsRegistered)
+                        {
+                            Machine.ValidateRegistration();
+                            return new WaitingForRegistrationSync(Machine, @event);
+                        }
+
+                        if (device.GetRegistrationToken() != null)
+                        {
+                            Machine.AddToEventQueue(new GotPushDeviceDetails());
+                        }
+                        else
+                        {
+                            Machine.GetRegistrationToken();
+                        }
+
+                        if (device.IsCreated == false)
+                        {
+                            Machine.CreateLocalDevice();
+                        }
+
+                        return new WaitingForPushDeviceDetails(Machine);
+                    case GotPushDeviceDetails _:
+                        return this;
+                    default:
+                        return null;
+                }
+            }
+        }
+
+        // Stub for now
+        public sealed class WaitingForPushDeviceDetails : State
+        {
+            public override bool Persist => true;
+
+            public WaitingForPushDeviceDetails(ActivationStateMachine machine)
+                : base(machine)
+            {
+            }
+
+            public override async Task<State> Transition(Event @event)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        // Stub for now
+        public sealed class WaitingForRegistrationSync : State
+        {
+            public override bool Persist => false;
+
+            private ActivationStateMachine.Event _fromEvent;
+
+            public WaitingForRegistrationSync(ActivationStateMachine machine, Event fromEvent)
+                : base(machine)
+            {
+                _fromEvent = fromEvent;
+            }
+
+            public override async Task<State> Transition(Event @event)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
