d6897d4e68cb2904d1c15a1d73cea7001ac0b600
Martin Georgiev <marto83@gmail.com>
2021-06-22 11:27:59 +0100

    Create PendingEvents queue and stub methods required for NotActivated State `Transition()` method

--- src/IO.Ably.Shared/Push/ActivationStateMachine.cs
+++ src/IO.Ably.Shared/Push/ActivationStateMachine.cs
@@ -1,14 +1,49 @@
-﻿namespace IO.Ably.Push
+﻿using System.Collections.Generic;
+
+namespace IO.Ably.Push
 {
     internal partial class ActivationStateMachine
     {
         private readonly AblyRest _restClient;
         private readonly ILogger _logger;
 
+        private Queue<Event> PendingEvents { get; set; } = new Queue<Event>();
+
         internal ActivationStateMachine(AblyRest restClient, ILogger logger)
         {
             _restClient = restClient;
             _logger = logger;
         }
+
+        public LocalDevice LocalDevice { get; set; }
+
+        private void CallDeactivatedCallback(object o)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void ValidateRegistration()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void AddToEventQueue(Event @event)
+        {
+            PendingEvents.Enqueue(@event);
+        }
+
+        private void GetRegistrationToken()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void CreateLocalDevice()
+        {
+            throw new System.NotImplementedException();
+        }
     }
+
+
+
+
 }
