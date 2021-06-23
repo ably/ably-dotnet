5d154fafc99941e690de6f94e6786efae556a732
Martin Georgiev <marto83@gmail.com>
2021-06-23 17:35:53 +0100

    Add Transition logic for WaitingForPushDeviceDetails

--- src/IO.Ably.Shared/Push/ActivationStateMachine.States.cs
+++ src/IO.Ably.Shared/Push/ActivationStateMachine.States.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace IO.Ably.Push
@@ -76,6 +77,77 @@ namespace IO.Ably.Push
             public override bool Persist => true;
 
             public WaitingForPushDeviceDetails(ActivationStateMachine machine)
+                : base(machine)
+            {
+            }
+
+            public override async Task<State> Transition(Event @event)
+            {
+                switch (@event)
+                {
+                    case CalledActivate _:
+                        return this;
+                    case CalledDeactivate _:
+                        Machine.CallDeactivatedCallback(null);
+                        return new NotActivated(Machine);
+                    case ActivationStateMachine.GettingPushDeviceDetailsFailed failedEvent:
+                        Machine.CallDeactivatedCallback(failedEvent.Reason);
+                        return new NotActivated(Machine);
+                    case GotPushDeviceDetails _:
+                    {
+                        DeviceDetails device = Machine.LocalDevice;
+
+                        var ably = Machine._restClient; // TODO: Check if there is an instance when Ably is not set. In which case Java set queues GettingDeviceRegistrationFailed
+
+                        // TODO: Make async
+                        try
+                        {
+                            var registeredDevice = await ably.Push.Admin.RegisterDevice(device);
+                            var deviceIdentityToken = registeredDevice.DeviceIdentityToken;
+                            if (deviceIdentityToken.IsEmpty())
+                            {
+                                // TODO: Log
+                                _ = Machine.HandleEvent(new GettingDeviceRegistrationFailed(new ErrorInfo("Invalid deviceIdentityToken in response", 40000, HttpStatusCode.BadRequest)));
+                            }
+                            else
+                            {
+                                // TODO: When integration testing this will most likely fail. I suspect deviceIdentityToken is not a plain string.
+                                _ = Machine.HandleEvent(new GotDeviceRegistration(deviceIdentityToken));
+
+                                // TODO: RSH8f. Leaving commented out code as a reminder.
+                                // I still haven't figured out how clientId in the state machine could be different from the one stored in the client
+                                // JsonPrimitive responseClientIdJson = response.getAsJsonPrimitive("clientId");
+                                // if (responseClientIdJson != null)
+                                // {
+                                //     String responseClientId = responseClientIdJson.getAsString();
+                                //     if (device.clientId == null)
+                                //     {
+                                //         /* Spec RSH8f: there is an implied clientId in our credentials that we didn't know about */
+                                //         activationContext.setClientId(responseClientId, false);
+                                //     }
+                                // }
+                            }
+                        }
+                        catch (AblyException e)
+                        {
+                            // Log
+                            _ = Machine.HandleEvent(new GettingDeviceRegistrationFailed(e.ErrorInfo));
+                        }
+
+                        return new WaitingForDeviceRegistration(Machine);
+                    }
+
+                    default:
+                        return null;
+                }
+            }
+        }
+
+        public sealed class WaitingForDeviceRegistration : State
+        {
+            public override bool Persist => false;
+
+            public WaitingForDeviceRegistration(ActivationStateMachine machine)
                 : base(machine)
             {
             }
