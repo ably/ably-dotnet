6e0997fdb698edbdab4367bf88c4f7bd74b1992e
Martin Georgiev <marto83@gmail.com>
2021-06-25 12:58:37 +0100

    Add DotNetPush App based on xamarin sample app

---
+++ examples/DotnetPush/DotnetPush/ViewModels/AboutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace DotnetPush.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public AboutViewModel()
+        {
+            Title = "About";
+            ActivatePush = new Command(() => Ably.Push.Activate());
+            DeactivatePush = new Command(() => Ably.Push.Deactivate());
+        }
+
+        public ICommand ActivatePush { get; }
+        public ICommand DeactivatePush { get; }
+    }
+}
