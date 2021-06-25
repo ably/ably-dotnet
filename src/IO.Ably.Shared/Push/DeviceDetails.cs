a93325029ecc0f83f0ac97696d424685b335cc3c
Martin Georgiev <marto83@gmail.com>
2021-06-25 13:04:08 +0100

    Add json attributes to DeviceDetails
    I also update DeviceFormFactor to be a string so I don't have to worry about serialization and deserialization from Json

--- src/IO.Ably.Shared/Push/DeviceDetails.cs
+++ src/IO.Ably.Shared/Push/DeviceDetails.cs
@@ -1,51 +1,54 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
 
 namespace IO.Ably.Push
 {
     /// <summary>
     /// Describes possible device form factors.
     /// </summary>
-    public enum DeviceFormFactor
+    public static class DeviceFormFactor
     {
         /// <summary>
-        /// Phone
+        /// Phone.
         /// </summary>
-        Phone,
+        public const string Phone = "phone";
 
         /// <summary>
-        /// Tablet
+        /// Tablet.
         /// </summary>
-        Tablet,
+        public const string Tablet = "tablet";
 
         /// <summary>
-        /// Desktop
+        /// Desktop.
         /// </summary>
-        Desktop,
+        public const string Desktop = "desktop";
 
         /// <summary>
-        /// Tv
+        /// Tv.
         /// </summary>
-        Tv,
+        public const string Tv = "tv";
 
         /// <summary>
-        /// Watch
+        /// Watch.
         /// </summary>
-        Watch,
+        public const string Watch = "watch";
 
         /// <summary>
-        /// Car
+        /// Car.
         /// </summary>
-        Car,
+        public const string Car = "car";
 
         /// <summary>
-        /// Embedded
+        /// Embedded.
         /// </summary>
-        Embedded,
+        public const string Embedded = "embedded";
 
         /// <summary>
-        /// Other
+        /// Other.
         /// </summary>
-        Other
+        public const string Other = "other";
     }
 
     /// <summary>
@@ -56,36 +59,43 @@ namespace IO.Ably.Push
         /// <summary>
         /// Device Id.
         /// </summary>
+        [JsonProperty("id")]
         public string Id { get; set; }
 
         /// <summary>
         /// Device platform (android|ios|web). TODO: Double check.
         /// </summary>
+        [JsonProperty("platform")]
         public string Platform { get; set; }
 
         /// <summary>
         /// Device form factor. <see cref="FormFactor"/>.
         /// </summary>
-        public DeviceFormFactor FormFactor { get; set; }
+        [JsonProperty("formFactor")]
+        public string FormFactor { get; set; }
 
         /// <summary>
         /// Device ClientId. TODO: Explain how this can be used to send push notifications.
         /// </summary>
+        [JsonProperty("clientId")]
         public string ClientId { get; set; }
 
         /// <summary>
         /// Device Metadata. TODO: Give example how this can be used.
         /// </summary>
+        [JsonProperty("metadata")]
         public JObject Metadata { get; set; }
 
         /// <summary>
         /// Push registration data. TODO: Describe how it is relevant to each platform and how it differs.
         /// </summary>
+        [JsonProperty("push")]
         public PushData Push { get; set; } = new PushData();
 
         /// <summary>
         /// Device secret. TODO: Describe how this could be used to authenticate push messages.
         /// </summary>
+        [JsonProperty("deviceSecret")]
         public string DeviceSecret { get; set; }
 
         /// <summary>
@@ -96,16 +106,19 @@ namespace IO.Ably.Push
             /// <summary>
             /// Push Recipient. TODO: // describe the different options.
             /// </summary>
+            [JsonProperty("recipient")]
             public JObject Recipient { get; set; } // TODO: Once we know all the variations of the data, I'd like to make it strongly typed.
 
             /// <summary>
             /// State. TODO: Add examples.
             /// </summary>
+            [JsonProperty("state")]
             public string State { get; set; }
 
             /// <summary>
             /// Error registering device as a PushTarget.
             /// </summary>
+            [JsonProperty("errorReason")]
             public ErrorInfo ErrorReason { get; set; }
         }
     }
