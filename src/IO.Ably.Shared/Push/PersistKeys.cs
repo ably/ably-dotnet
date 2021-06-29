db37e5028bc4acc000afc4aabbb51f3d426ee0b9
Martin Georgiev <marto83@gmail.com>
2021-06-29 10:31:48 +0100

    Remove unused key

--- src/IO.Ably.Shared/Push/PersistKeys.cs
+++ src/IO.Ably.Shared/Push/PersistKeys.cs
@@ -6,7 +6,6 @@
         {
             public const string SharedName = "Ably_StateMachine";
             public const string CURRENT_STATE = "ABLY_PUSH_CURRENT_STATE";
-            public const string PENDING_EVENTS_LENGTH = "ABLY_PUSH_PENDING_EVENTS_LENGTH";
             public const string PENDING_EVENTS = "ABLY_PUSH_PENDING_EVENTS";
             public const string PUSH_CUSTOM_REGISTRAR = "ABLY_PUSH_REGISTRATION_HANDLER";
         }
