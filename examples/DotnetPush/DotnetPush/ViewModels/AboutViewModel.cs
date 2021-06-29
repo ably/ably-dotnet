9e76103a8b457d0a93486b59ce1e4a7cba6df3ab
Martin Georgiev <marto83@gmail.com>
2021-06-29 15:43:02 +0100

    Update About page with buttons and info about the current ClientId and StateMachine state

--- examples/DotnetPush/DotnetPush/ViewModels/AboutViewModel.cs
+++ examples/DotnetPush/DotnetPush/ViewModels/AboutViewModel.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Windows.Input;
-using Xamarin.Essentials;
+﻿using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace DotnetPush.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        private string _clientId;
+        private string _currentState;
+
         public AboutViewModel()
         {
             Title = "About";
             var pushRealtime = Ably.GetPushRealtime();
+            ClientId = Ably.Auth.ClientId;
+            Ably.GetPushRealtime().OnActivationStateMachineChangeState((current, next) =>
+            {
+                CurrentState = next;
+            });
 
             ActivatePush = new Command(() => pushRealtime.Activate());
             DeactivatePush = new Command(() => pushRealtime.Deactivate());
@@ -18,5 +24,17 @@ namespace DotnetPush.ViewModels
 
         public ICommand ActivatePush { get; }
         public ICommand DeactivatePush { get; }
+
+        public string ClientId
+        {
+            get => _clientId;
+            set => SetProperty(ref _clientId, value);
+        }
+
+        public string CurrentState
+        {
+            get => _currentState;
+            set => SetProperty(ref _currentState, value);
+        }
     }
 }
