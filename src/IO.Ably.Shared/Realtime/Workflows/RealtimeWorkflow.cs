d1bc14af7f60b03b73f60655863f44b4d70225a4
Martin Georgiev <marto83@gmail.com>
2021-06-30 18:08:22 +0100

    Remove obsolete code.

--- src/IO.Ably.Shared/Realtime/Workflows/RealtimeWorkflow.cs
+++ src/IO.Ably.Shared/Realtime/Workflows/RealtimeWorkflow.cs
@@ -226,7 +226,6 @@ namespace IO.Ably.Realtime.Workflow
                             return new RealtimeCommand[] { CompleteWorkflowCommand.Create().TriggeredBy(command) };
                         }
 
-                        break;
                     case CompleteWorkflowCommand _:
                         _heartbeatMonitorCancellationTokenSource.Cancel();
                         Channels.ReleaseAll();
