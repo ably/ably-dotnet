b69f5cc0f8d1ee24634b6b7ca7053e91e64456a2
Martin Georgiev <marto83@gmail.com>
2021-06-30 11:55:05 +0100

    Add logging to AndroidMobileDevice

--- src/IO.Ably.Push.Android/AndroidMobileDevice.cs
+++ src/IO.Ably.Push.Android/AndroidMobileDevice.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Runtime.InteropServices;
 using Android.App;
 using Android.Content;
 using Android.Gms.Tasks;
 using Android.Support.V4.Content;
+using Android.Views;
 using Firebase.Messaging;
 using IO.Ably.Infrastructure;
 using Xamarin.Essentials;
@@ -16,11 +18,13 @@ namespace IO.Ably.Push.Android
     {
         public static void Initialise()
         {
-            IoC.MobileDevice = new AndroidMobileDevice(null);
+            IoC.MobileDevice = new AndroidMobileDevice(DefaultLogger.LoggerInstance);
         }
 
         public static void OnNewRegistrationToken(string token)
         {
+            var logger = DefaultLogger.LoggerInstance;
+            logger.Debug($"Received OnNewRegistrationToken with token {token}");
             //Get Ably and the ActivationStateMachine
         }
 
@@ -72,6 +76,8 @@ namespace IO.Ably.Push.Android
                 throw new ArgumentException("Please provide name when sending intent.", nameof(name));
             }
 
+            _logger.Debug($"Sending intent {name}");
+
             var action = "io.ably.broadcast." + name.ToLower();
             try
             {
@@ -89,13 +95,13 @@ namespace IO.Ably.Push.Android
             catch (Exception e)
             {
                 _logger.Error($"Error sending intent {action}", e);
-                // Log
                 throw new AblyException(e);
             }
         }
 
         public void SetPreference(string key, string value, string groupName)
         {
+            _logger.Debug($"Setting preferences: {groupName}:{key} with value {value}");
             Preferences.Set(key, value, groupName);
         }
 
@@ -106,11 +112,13 @@ namespace IO.Ably.Push.Android
 
         public void RemovePreference(string key, string groupName)
         {
+            _logger.Debug($"Removing preference: {groupName}:{key}");
             Preferences.Remove(key, groupName);
         }
 
         public void ClearPreferences(string groupName)
         {
+            _logger.Debug($"Clearing preferences group: {groupName}");
             Preferences.Clear(groupName);
         }
 
@@ -118,14 +126,15 @@ namespace IO.Ably.Push.Android
         {
             try
             {
+                _logger.Debug("Requesting a new Registration token");
                 var messagingInstance = FirebaseMessaging.Instance;
                 var resultTask = messagingInstance.GetToken();
 
-                resultTask.AddOnCompleteListener(new RequestTokenCompleteListener(callback));
+                resultTask.AddOnCompleteListener(new RequestTokenCompleteListener(callback, _logger));
             }
             catch (Exception e)
             {
-                // TODO: Log
+                _logger.Error("Error while requesting a new Registration token.", e);
                 var errorInfo = new ErrorInfo($"Failed to request AndroidToken. Error: {e?.Message}.", 50000,
                     HttpStatusCode.InternalServerError, e);
                 callback(Result.Fail<string>(errorInfo));
@@ -136,21 +145,24 @@ namespace IO.Ably.Push.Android
         public class RequestTokenCompleteListener : Java.Lang.Object, IOnCompleteListener
         {
             private readonly Action<Result<string>> _callback;
+            private readonly ILogger _logger;
 
-            public RequestTokenCompleteListener(Action<Result<string>> callback)
+            public RequestTokenCompleteListener(Action<Result<string>> callback, ILogger logger)
             {
                 _callback = callback;
+                _logger = logger;
             }
 
             public void OnComplete(Task task)
             {
                 if (task.IsSuccessful)
                 {
+                    _logger.Debug($"RequestTaken completed successfully. Result: {task.Result}");
                     _callback(Result.Ok((string) task.Result));
                 }
                 else
                 {
-                    // TODO: Log
+                    _logger.Error("RequestToken failed.", task.Exception);
                     var exception = task.Exception;
                     var errorInfo = new ErrorInfo($"Failed to return valid AndroidToken. Error: {exception?.Message}.",
                         50000, HttpStatusCode.InternalServerError, exception);
