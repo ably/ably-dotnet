3c22103818f2e4a3dcf60b4b7d99d93e3e1d53e8
Martin Georgiev <marto83@gmail.com>
2021-07-14 11:56:12 +0100

    Add validation + validation test for RSH1a

---
+++ src/IO.Ably.Tests.Shared/Push/PushAdminTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IO.Ably.Tests.DotNetCore20.Push
+{
+    [Trait("spec", "RSH1")]
+    public class PushAdminTests : MockHttpRestSpecs
+    {
+        [Fact]
+        [Trait("spec", "RSH1a")]
+        public async Task Publish_WithEmptyRecipientOrData_ShouldThrow()
+        {
+            var rest = GetRestClient();
+
+            var recipientEx = await Assert.ThrowsAsync<AblyException>(() => rest.Push.Admin.PublishAsync(null, new JObject()));
+            recipientEx.ErrorInfo.Code.Should().Be(ErrorCodes.BadRequest);
+
+            var dataEx = await Assert.ThrowsAsync<AblyException>(() => rest.Push.Admin.PublishAsync(new JObject(), null));
+            dataEx.ErrorInfo.Code.Should().Be(ErrorCodes.BadRequest);
+        }
+
+        public PushAdminTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+    }
+}
