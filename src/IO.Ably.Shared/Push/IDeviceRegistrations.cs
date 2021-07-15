53918346d41894ef89d662bfee0bdd160faa0107
Martin Georgiev <marto83@gmail.com>
2021-07-15 14:50:36 +0100

    Update PushAdmin List DeviceRegistrations + add RSH1b2 test

--- src/IO.Ably.Shared/Push/IDeviceRegistrations.cs
+++ src/IO.Ably.Shared/Push/IDeviceRegistrations.cs
@@ -27,11 +27,9 @@ namespace IO.Ably.Push
         /// Obtain the details for devices registered for receiving push registrations.
         /// RestApi: https://ably.com/documentation/rest-api#list-device-registrations.
         /// </summary>
-        /// <param name="clientId">Optional filter by clientId. DeviceId and ClientId cannot be combined.</param>
-        /// <param name="deviceId">Optional filter by deviceId. DeviceId and ClientId cannot be combined.</param>
-        /// <param name="limit">Number of results returned. Max allowed value: 1000.</param>
+        /// <param name="request">Allows to filter by clientId or deviceId. For further information <see cref="ListDeviceDetailsRequest"/>.</param>
         /// <returns>A paginated list of DeviceDetails.</returns>
-        Task<PaginatedResult<DeviceDetails>> List(string clientId = null, string deviceId = null, int? limit = null);
+        Task<PaginatedResult<DeviceDetails>> List(ListDeviceDetailsRequest request);
 
         /// <summary>
         /// Removes a registered device.
@@ -48,7 +46,5 @@ namespace IO.Ably.Push
         /// <param name="deviceId">The deviceId of the device to be removed.</param>
         /// <returns>Task.</returns>
         Task RemoveAsync(string deviceId);
-
-        // TODO: Discuss with Tom whether to add more helper methods to cover the full Rest Api.
     }
 }
