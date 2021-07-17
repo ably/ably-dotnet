5ecccbac71483662d3546aef5ff48551d0fe7380
Tom Kirby-Green <tomkirbygreen@gmail.com>
2021-07-17 14:50:31 +0100

    'AblyInsecureRequestException' Code Analysis Fixes

---
+++ src/IO.Ably.Tests.Shared/AblyInsecureRequestExceptionTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using FluentAssertions;
+using Xunit;
+
+namespace IO.Ably.Tests.Shared
+{
+    public class AblyInsecureRequestExceptionTests
+    {
+        [Fact]
+        public void Construct_WithMessage()
+        {
+            var e = new AblyInsecureRequestException("Something bad happened.");
+            e.Message.Should().Contain("Something bad happened.");
+        }
+
+        [Fact]
+        public void Construct_WithMessageAndInnerException()
+        {
+            var inner = new Exception("Troll in the dungeon!");
+            var e = new AblyInsecureRequestException("Something bad happened.", inner);
+            e.Message.Should().Contain("Something bad happened.");
+            e.InnerException.Should().Be(inner);
+        }
+    }
+}
