3dc6f3a668f6084c4d20ef53dcb605892c0f6411
Martin Georgiev <marto83@gmail.com>
2021-07-21 10:16:20 +0100

    Update PersistKeys to remove screaming caps

--- src/IO.Ably.Shared/Push/PersistKeys.cs
+++ src/IO.Ably.Shared/Push/PersistKeys.cs
@@ -5,22 +5,21 @@
         public static class StateMachine
         {
             public const string SharedName = "Ably_StateMachine";
-            public const string CURRENT_STATE = "ABLY_PUSH_CURRENT_STATE";
-            public const string PENDING_EVENTS_LENGTH = "ABLY_PUSH_PENDING_EVENTS_LENGTH";
-            public const string PENDING_EVENTS = "ABLY_PUSH_PENDING_EVENTS";
-            public const string PUSH_CUSTOM_REGISTRAR = "ABLY_PUSH_REGISTRATION_HANDLER";
+            public const string CurrentState = "ABLY_PUSH_CURRENT_STATE";
+            public const string PendingEvents = "ABLY_PUSH_PENDING_EVENTS";
+            public const string PushCustomRegistrar = "ABLY_PUSH_REGISTRATION_HANDLER";
         }
 
         public static class Device
         {
             public const string SharedName = "Ably_Device";
 
-            public const string DEVICE_ID = "ABLY_DEVICE_ID";
-            public const string CLIENT_ID = "ABLY_CLIENT_ID";
-            public const string DEVICE_SECRET = "ABLY_DEVICE_SECRET";
-            public const string DEVICE_TOKEN = "ABLY_DEVICE_IDENTITY_TOKEN";
-            public const string TOKEN_TYPE = "ABLY_REGISTRATION_TOKEN_TYPE";
-            public const string TOKEN = "ABLY_REGISTRATION_TOKEN";
+            public const string DeviceId = "ABLY_DEVICE_ID";
+            public const string ClientId = "ABLY_CLIENT_ID";
+            public const string DeviceSecret = "ABLY_DEVICE_SECRET";
+            public const string DeviceToken = "ABLY_DEVICE_IDENTITY_TOKEN";
+            public const string TokenType = "ABLY_REGISTRATION_TOKEN_TYPE";
+            public const string Token = "ABLY_REGISTRATION_TOKEN";
         }
     }
 }
