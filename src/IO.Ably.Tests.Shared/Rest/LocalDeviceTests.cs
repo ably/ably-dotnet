e465365cecfea0cf2f0bc10270f6b515cc0a4756
Martin Georgiev <marto83@gmail.com>
2021-09-24 12:18:12 +0100

    Add tests to make sure Realtime.Device and Rest.Device return an instance when MobileDevice is present

---
+++ src/IO.Ably.Tests.Shared/Rest/LocalDeviceTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using IO.Ably.Tests.Push;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IO.Ably.Tests.DotNetCore20.Rest
+{
+    public class LocalDeviceTests : MockHttpRestSpecs
+    {
+        [Fact]
+        [Trait("spec", "RSH8")]
+        public void WhenPlatformsSupportsPushNotifications_ShouldBeAbleToRetrieveLocalDeviceFromRestClient()
+        {
+            var mobileDevice = new FakeMobileDevice();
+
+            var rest = GetRestClient(mobileDevice: mobileDevice);
+
+            rest.Device.Should().NotBeNull();
+        }
+
+        [Fact]
+        [Trait("spec", "RSH8")]
+        public void WhenPlatformsSupportsPushNotifications_ShouldBeAbleToRetrieveLocalDeviceFromRealtimeClient()
+        {
+            var mobileDevice = new FakeMobileDevice();
+
+            var options = new ClientOptions(ValidKey)
+            {
+                AutoConnect = false
+            };
+
+            var realtime = new AblyRealtime(options, mobileDevice: mobileDevice);
+
+            realtime.Device.Should().NotBeNull();
+        }
+
+        public LocalDeviceTests(ITestOutputHelper output)
+            : base(output)
+        {
+        }
+    }
+}
