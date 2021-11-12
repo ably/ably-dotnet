9326705e84d3484dba41cc576bd03468031542cf
Martin Georgiev <marto83@gmail.com>
2021-11-12 17:58:30 +0000

    Remove InternalsVisibleTo from IO.Ably.iOS to fix the build.
    The error was due to the fact that the iOS library needs the assemblies to be signed with a strong key which we don't yet have.
    We should remove the specific Xamarin ably packages to force the usage of the .netstandard package.

--- src/IO.Ably.iOS/Properties/AssemblyInfo.cs
+++ src/IO.Ably.iOS/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -8,5 +7,3 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("IO.Ably.Push.Android")]
-[assembly: InternalsVisibleTo("IO.Ably.Push.iOS")]
