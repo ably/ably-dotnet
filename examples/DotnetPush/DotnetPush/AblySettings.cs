5a63d4e2d112aab058c2b43c1e2c5b326e6a6e6a
Martin Georgiev <marto83@gmail.com>
2021-11-23 22:22:12 +0000

    Removed unused property

--- examples/DotnetPush/DotnetPush/AblySettings.cs
+++ examples/DotnetPush/DotnetPush/AblySettings.cs
@@ -18,14 +18,5 @@ namespace DotnetPush
             get => Preferences.Get(ClientIdPreferenceKey, string.Empty, "Ably_Device");
             set => Preferences.Set(ClientIdPreferenceKey, value, "Ably_Device");
         }
-
-        /// <summary>
-        /// Ably key.
-        /// </summary>
-        public static string AblyKey
-        {
-            get => Preferences.Get(AuthKeyPreferenceKey, string.Empty, "Ably_Device");
-            set => Preferences.Set(AuthKeyPreferenceKey, value, "Ably_Device");
-        }
     }
 }
