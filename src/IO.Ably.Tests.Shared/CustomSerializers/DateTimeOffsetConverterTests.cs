0d1fb8b882bf5cc6cb950881e178e5c66096bfd7
sacOO7 <sachinshinde7676@gmail.com>
2021-12-27 18:18:40 +0530

    Added nullableDateTimeOffset test cases

--- src/IO.Ably.Tests.Shared/CustomSerializers/DateTimeOffsetConverterTests.cs
+++ src/IO.Ably.Tests.Shared/CustomSerializers/DateTimeOffsetConverterTests.cs
@@ -18,24 +18,14 @@ namespace IO.Ably.Tests.Shared.CustomSerializers
             public DateTimeOffset DateTimeOffset { get; set; }
         }
 
-        public class NullableTestClass
-        {
-            public NullableTestClass(DateTimeOffset? data = null)
-            {
-                DateTimeOffset = data;
-            }
-
-            public DateTimeOffset? DateTimeOffset { get; set; }
-        }
-
         [Fact]
         public void ShouldConvertDateTimeOffsetToMilliseconds()
         {
             var originalDateTimeOffset = new TestClass(new DateTimeOffset(2014, 1, 1, 0, 0, 0, TimeSpan.Zero));
             var serializedDateTimeOffset = JsonHelper.Serialize(originalDateTimeOffset);
             var serializedJToken = JToken.Parse(serializedDateTimeOffset);
-            serializedJToken["DateTimeOffset"].Should().NotBeNull();
-            serializedJToken["DateTimeOffset"].Type.Should().Be(JTokenType.Integer);
+            serializedJToken["DateTimeOffset"].Should().NotBeNull(); // check that json property/key exists
+            serializedJToken["DateTimeOffset"].Type.Should().Be(JTokenType.Integer); // check type of the property
             serializedJToken["DateTimeOffset"].Value<long>().Should().Be(1388534400000);
         }
 
@@ -53,19 +43,44 @@ namespace IO.Ably.Tests.Shared.CustomSerializers
         [Fact]
         public void ShouldSetMinDateTimeOffsetForExcludedProperty()
         {
+            var originalDateTimeOffset = new NullableTestClass();
+            var serializedDateTimeOffset = JsonHelper.Serialize(originalDateTimeOffset);
+            var serializedJToken = JToken.Parse(serializedDateTimeOffset);
+            serializedJToken["DateTimeOffset"].Should().BeNull();
 
+            var deserializedDateTimeOffset = JsonHelper.Deserialize<TestClass>(serializedDateTimeOffset);
+            deserializedDateTimeOffset.DateTimeOffset.Should().Be(DateTimeOffset.MinValue);
+        }
+
+        public class NullableTestClass
+        {
+            public NullableTestClass(DateTimeOffset? data = null)
+            {
+                DateTimeOffset = data;
+            }
+
+            public DateTimeOffset? DateTimeOffset { get; set; }
         }
 
         [Fact]
         public void ShouldExcludeNullDateTimeOffset()
         {
-
+            var originalDateTimeOffset = new NullableTestClass();
+            var serializedDateTimeOffset = JsonHelper.Serialize(originalDateTimeOffset);
+            var serializedJToken = JToken.Parse(serializedDateTimeOffset);
+            serializedJToken["DateTimeOffset"].Should().BeNull();
         }
 
         [Fact]
         public void ShouldSetNullForExcludedProperty()
         {
+            var originalDateTimeOffset = new NullableTestClass();
+            var serializedDateTimeOffset = JsonHelper.Serialize(originalDateTimeOffset);
+            var serializedJToken = JToken.Parse(serializedDateTimeOffset);
+            serializedJToken["DateTimeOffset"].Should().BeNull();
 
+            var deserializedDateTimeOffset = JsonHelper.Deserialize<NullableTestClass>(serializedDateTimeOffset);
+            deserializedDateTimeOffset.DateTimeOffset.Should().BeNull();
         }
     }
 }
