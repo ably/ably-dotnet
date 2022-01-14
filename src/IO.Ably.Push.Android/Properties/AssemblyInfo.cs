5d30e0672898608b893a6dd07b47eb8769bd9521
Martin Georgiev <marto83@gmail.com>
2022-01-14 10:07:45 +0000

    Add CommandAssembly info to Android and iOS

--- src/IO.Ably.Push.Android/Properties/AssemblyInfo.cs
+++ src/IO.Ably.Push.Android/Properties/AssemblyInfo.cs
@@ -1,7 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
@@ -9,22 +6,3 @@ using Android.App;
 [assembly: AssemblyTitle("IO.Ably.Push.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IO.Ably.Push.Android")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
