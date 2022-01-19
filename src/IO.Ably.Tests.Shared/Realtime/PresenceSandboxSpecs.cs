31e5bcdee0101c36ed474182dee34bf1db144dff
sacOO7 <sachinshinde7676@gmail.com>
2022-01-19 20:10:48 +0530

    Extracted sync action block in a separate method

--- src/IO.Ably.Tests.Shared/Realtime/PresenceSandboxSpecs.cs
+++ src/IO.Ably.Tests.Shared/Realtime/PresenceSandboxSpecs.cs
@@ -38,6 +38,11 @@ namespace IO.Ably.Tests.Realtime
             return GetChannel(client, channelNamePrefix.AddRandomSuffix());
         }
 
+        private static void BlockAblyServerSentSyncAction(IRealtimeClient client)
+        {
+            client.BlockActionFromReceiving(ProtocolMessage.MessageAction.Sync);
+        }
+
         private static RealtimeChannel GetChannel(IRealtimeClient client, string channelName)
         {
             var channel = client.Channels.Get(channelName) as RealtimeChannel;
@@ -141,8 +146,7 @@ namespace IO.Ably.Tests.Realtime
                 var client = await GetRealtimeClient(protocol);
                 await client.WaitForState(ConnectionState.Connected);
 
-                // Block ably server sent sync action
-                client.BlockActionFromReceiving(ProtocolMessage.MessageAction.Sync);
+                BlockAblyServerSentSyncAction(client);
 
                 var channel = client.Channels.Get(channelName);
                 await channel.AttachAsync();
@@ -699,8 +703,7 @@ namespace IO.Ably.Tests.Realtime
                 var client = await GetRealtimeClient(protocol);
                 await client.WaitForState(ConnectionState.Connected);
 
-                // Block ably server sent sync action
-                client.BlockActionFromReceiving(ProtocolMessage.MessageAction.Sync);
+                BlockAblyServerSentSyncAction(client);
 
                 var channel = client.Channels.Get(channelName);
                 await channel.AttachAsync();
