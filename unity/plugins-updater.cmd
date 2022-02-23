749ea8e4491286295811d74b47bbdee2ffe4f903
sacOO7 <sachinshinde7676@gmail.com>
2022-02-23 16:34:47 +0530

    Updated error message for plugin updator scripts with no version provided

--- unity/plugins-updater.cmd
+++ unity/plugins-updater.cmd
@@ -1,5 +1,5 @@
 @echo off
-if "%~1"=="" (echo "Provide latest version number like plugins-updator.cmd 1.2.8") else (
+if "%~1"=="" (echo "Provide latest version number like plugins-updater.cmd 1.2.8") else (
 	dotnet fake run ..\build.fsx -t Build.NetStandard
 	copy ..\src\IO.Ably.NETStandard20\bin\Release\netstandard2.0\IO.Ably.dll Assets\Ably\Plugins
 	copy ..\src\IO.Ably.NETStandard20\bin\Release\netstandard2.0\IO.Ably.pdb Assets\Ably\Plugins 
--- unity/plugins-updater.sh
+++ unity/plugins-updater.sh
@@ -1,6 +1,6 @@
 if [ $# -eq 0 ]
 then
-  echo -n "Provide latest version number like plugins-updator.cmd 1.2.8"
+  echo -n "Provide latest version number like plugins-updater.sh 1.2.8"
 else
 	dotnet fake run ../build.fsx -t Build.NetStandard
 	cp ../src/IO.Ably.NETStandard20/bin/Release/netstandard2.0/IO.Ably.dll Assets/Ably/Plugins
