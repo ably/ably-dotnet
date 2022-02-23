96826b0657bd0fc0fc6ba93d23499400421309b3
sacOO7 <sachinshinde7676@gmail.com>
2022-02-23 14:46:34 +0530

    Added bash script to update plugins

---
+++ unity/plugins-updater.sh
@@ -0,0 +1,12 @@
+if [ $# -eq 0 ]
+then
+  echo -n "Provide latest version number like plugins-updator.cmd 1.2.8"
+else
+  	cd ../
+	dotnet fake run build.fsx -t Build.NetStandard
+	cp src/IO.Ably.NETStandard20/bin/Release/netstandard2.0/IO.Ably.dll unity/Assets/Ably/Plugins
+	cp src/IO.Ably.NETStandard20/bin/Release/netstandard2.0/IO.Ably.pdb unity/Assets/Ably/Plugins 
+	cp src/IO.Ably.NETStandard20/bin/Release/netstandard2.0/IO.Ably.DeltaCodec.dll unity/Assets/Ably/Plugins
+	cp src/IO.Ably.NETStandard20/bin/Release/netstandard2.0/IO.Ably.DeltaCodec.pdb unity/Assets/Ably/Plugins
+	echo $1 > unity/Assets/Ably/version.txt
+fi
