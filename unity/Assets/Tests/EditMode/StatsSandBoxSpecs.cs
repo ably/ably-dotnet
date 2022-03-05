8961f6d31cb42858bef9c398f7e53e72223256dc
sacOO7 <sachinshinde7676@gmail.com>
2022-03-06 00:18:03 +0530

    Added integration tests for reading stats

---
+++ unity/Assets/Tests/EditMode/StatsSandBoxSpecs.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assets.Tests.AblySandbox;
+using Cysharp.Threading.Tasks;
+using FluentAssertions;
+using IO.Ably;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace Assets.Tests.EditMode
+{
+    [TestFixture]
+    public class StatsSandBoxSpecs
+    {
+        private AblySandboxFixture _sandboxFixture;
+
+        [OneTimeSetUp]
+        public void OneTimeInit()
+        {
+            _sandboxFixture = new AblySandboxFixture();
+        }
+
+        [UnitySetUp]
+        public IEnumerator Init()
+        {
+            UnitySandbox = new UnitySandboxSpecs(_sandboxFixture);
+            yield return null;
+        }
+
+        [UnityTearDown]
+        public IEnumerator TearDown()
+        {
+            UnitySandbox.Dispose();
+            yield return null;
+        }
+
+        public UnitySandboxSpecs UnitySandbox { get; set; }
+
+
+        private static readonly DateTimeOffset StartInterval =
+            DateHelper.CreateDate(DateTimeOffset.UtcNow.Year - 1, 2, 3, 15, 5);
+
+        private async Task<List<Stats>> GetStats(Protocol protocol)
+        {
+            var client = await UnitySandbox.GetRestClient(protocol);
+            var result = await client.StatsAsync(new StatsRequestParams
+                {Start = StartInterval.AddMinutes(-2), End = StartInterval.AddMinutes(1)});
+
+            return result.Items;
+        }
+
+        static Protocol[] protocols = {Protocol.Json};
+
+        [Property("spec", "G3")]
+        [UnityTest]
+        public IEnumerator ShouldHaveCorrectStatsAsPerStatsSpec([ValueSource(nameof(protocols))] Protocol protocol) =>
+            UniTask.ToCoroutine(async () =>
+            {
+                await _sandboxFixture.SetupStats();
+
+                async Task GetAndValidateStats()
+                {
+                    var allStats = await GetStats(protocol);
+                    var stats = allStats.First();
+                    stats.All.Messages.Count.Should().Be(40 + 70);
+                    stats.All.Messages.Data.Should().Be(4000 + 7000);
+                    stats.Inbound.Realtime.All.Count.Should().Be(70);
+                    stats.Inbound.Realtime.All.Data.Should().Be(7000);
+                    stats.Inbound.Realtime.Messages.Count.Should().Be(70);
+                    stats.Inbound.Realtime.Messages.Data.Should().Be(7000);
+                    stats.Outbound.Realtime.All.Count.Should().Be(40);
+                    stats.Outbound.Realtime.All.Data.Should().Be(4000);
+                    stats.Persisted.Presence.Count.Should().Be(20);
+                    stats.Persisted.Presence.Data.Should().Be(2000);
+                    stats.Connections.Tls.Peak.Should().Be(20);
+                    stats.Connections.Tls.Opened.Should().Be(10);
+                    stats.Channels.Peak.Should().Be(50);
+                    stats.Channels.Opened.Should().Be(30);
+                    stats.ApiRequests.Succeeded.Should().Be(50);
+                    stats.ApiRequests.Failed.Should().Be(10);
+                    stats.TokenRequests.Succeeded.Should().Be(60);
+                    stats.TokenRequests.Failed.Should().Be(20);
+                }
+
+                await UnitySandbox.AssertMultipleTimes(GetAndValidateStats, 5, TimeSpan.FromSeconds(5));
+            });
+    }
+}
