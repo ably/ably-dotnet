2786b67b41fe30b793699f64108de24fb68f6e1b
sacOO7 <sachinshinde7676@gmail.com>
2022-03-06 11:30:57 +0530

    Moved unity-plugins-updater script at the root, refactored internal paths

--- unity/plugins-updater.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-if "%~1"=="" (echo "Provide latest version number like plugins-updater.cmd 1.2.8") else (
-	dotnet fake run ..\build.fsx -t Build.NetStandard
-	copy ..\src\IO.Ably.NETStandard20\bin\Release\netstandard2.0\IO.Ably.dll Assets\Ably\Plugins
-	copy ..\src\IO.Ably.NETStandard20\bin\Release\netstandard2.0\IO.Ably.pdb Assets\Ably\Plugins 
-	copy ..\src\IO.Ably.NETStandard20\bin\Release\netstandard2.0\IO.Ably.DeltaCodec.dll Assets\Ably\Plugins
-	copy ..\src\IO.Ably.NETStandard20\bin\Release\netstandard2.0\IO.Ably.DeltaCodec.pdb Assets\Ably\Plugins
-	echo %~1 > Assets\Ably\version.txt
-)
