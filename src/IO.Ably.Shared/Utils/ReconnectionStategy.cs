c03ec1e0b9ed1db628f20bcfdb50a42ff048c10c
sacOO7 <sachinshinde7676@gmail.com>
2022-08-08 15:38:03 +0530

    Added utils class related to reconnection

---
+++ src/IO.Ably.Shared/Utils/ReconnectionStategy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IO.Ably.Shared.Utils
+{
+    internal class ReconnectionStategy
+    {
+        public static double GetBackoffCoefficient(int retryCount)
+        {
+            return Math.Min((retryCount + 2) / 3, 2);
+        }
+
+        public static double GetJitterCoefficient()
+        {
+            return 1 - (new Random().NextDouble() * 0.2);
+        }
+
+        public static double GetRetryTime(double initValue, int retryCount)
+        {
+            return initValue * GetBackoffCoefficient(retryCount) * GetJitterCoefficient();
+        }
+    }
+}
