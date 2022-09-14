e159a36dcb5f07ab0b0755c2cc6bf9969cbb952a
sacOO7 <sachinshinde7676@gmail.com>
2022-09-14 13:14:02 +0530

    Added recovery context class to store recovery Key

---
+++ src/IO.Ably.Shared/Realtime/RecoveryKeyContext.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace IO.Ably.Shared.Realtime
+{
+    internal class RecoveryKeyContext
+    {
+        [JsonProperty("connectionKey")]
+        public string ConnectionKey { get; set; }
+
+        [JsonProperty("msgSerial")]
+        public long MsgSerial { get; set; }
+
+        [JsonProperty("channelSerials")]
+        public Dictionary<string, Dictionary<string, string>> ChannelSerials { get; set; }
+    }
+}
