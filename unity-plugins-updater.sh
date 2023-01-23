4f9c71e7cfd3cf2340c4c6df1d0e44eb1dbfc0bf
sacOO7 <sachinshinde7676@gmail.com>
2023-01-23 18:59:56 +0530

    Added unix and windows scripts for building DLL's for unity

---
+++ unity-plugins-updater.sh
@@ -0,0 +1,11 @@
+if [ $# -eq 0 ]
+then
+  echo -n "Provide latest version number like unity-plugins-updater.sh 1.2.8"
+else
+	dotnet fake run build.fsx -t Build.NetStandard
+	cp src/IO.Ably.NETStandard20/bin/Release/netstandard2.0/IO.Ably.dll unity/Assets/Ably/Plugins
+	cp src/IO.Ably.NETStandard20/bin/Release/netstandard2.0/IO.Ably.pdb unity/Assets/Ably/Plugins 
+	cp src/IO.Ably.NETStandard20/bin/Release/netstandard2.0/IO.Ably.DeltaCodec.dll unity/Assets/Ably/Plugins
+	cp src/IO.Ably.NETStandard20/bin/Release/netstandard2.0/IO.Ably.DeltaCodec.pdb unity/Assets/Ably/Plugins
+	echo $1 > unity/Assets/Ably/version.txt
+fi
