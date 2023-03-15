f796273513b7ec3d65d8a8b298e84a8e9c5deb4c
sacOO7 <sachinshinde7676@gmail.com>
2023-03-16 02:58:34 +0530

    Added platform identifiers for for dotnet 6 and dotnet 7

--- src/IO.Ably.NETStandard20/Platform.cs
+++ src/IO.Ably.NETStandard20/Platform.cs
@@ -16,7 +16,15 @@ namespace IO.Ably
 
         internal static bool HookedUpToNetworkEvents { get; private set; }
 
+        // Defined as per https://learn.microsoft.com/en-us/dotnet/standard/frameworks#preprocessor-symbols
+        // TODO : Get runtime platform info ( android, iOS ) using https://learn.microsoft.com/en-us/dotnet/maui/platform-integration/device/information?view=net-maui-7.0&tabs=windows
+#if NET6_0
+        public string PlatformId => "net6.0";
+#elif NET7_0
+        public string PlatformId => "net7.0";
+#else
         public string PlatformId => "netstandard20";
+#endif
 
         public ITransportFactory TransportFactory => null;
 
