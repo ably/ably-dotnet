8b9c0d3f9570d72e21e4dbd6a0cf13da39954d82
sacOO7 <sachinshinde7676@gmail.com>
2023-04-14 01:30:25 +0530

    Added comments to process OS specific agent headers

--- src/IO.Ably.NETStandard20/Platform.cs
+++ src/IO.Ably.NETStandard20/Platform.cs
@@ -18,6 +18,9 @@ namespace IO.Ably
 
         // Defined as per https://learn.microsoft.com/en-us/dotnet/standard/frameworks#preprocessor-symbols
         // TODO : Get runtime platform info ( android, iOS ) using https://learn.microsoft.com/en-us/dotnet/maui/platform-integration/device/information?view=net-maui-7.0&tabs=windows
+        // TODO : Get all operating system from https://stackoverflow.com/questions/38790802/determine-operating-system-in-net-core
+        // System.OperatingSystem.IsWindows(), System.OperatingSystem.IsIOS()
+        // Also, update OS detection for dotnetcore
 #if NET6_0
         public string PlatformId => "net6.0";
 #elif NET7_0
