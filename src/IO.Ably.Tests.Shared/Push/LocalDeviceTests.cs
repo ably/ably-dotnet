9afbfdf76bbce6c0e28ad0fb63ecf85b00c8057c
sacOO7 <sachinshinde7676@gmail.com>
2023-08-13 01:53:12 +0530

    Unskipped localdevice flaky tests

--- src/IO.Ably.Tests.Shared/Push/LocalDeviceTests.cs
+++ src/IO.Ably.Tests.Shared/Push/LocalDeviceTests.cs
@@ -132,7 +132,7 @@ namespace IO.Ably.Tests.Push
             rest.MobileDevice.Should().BeNull();
         }
 
-        [Fact(Skip = "Intermittently fails")]
+        [Fact]
         [Trait("spec", "RSH8a")]
         public void LocalDevice_IsOnlyInitialisedOnce()
         {
@@ -157,7 +157,7 @@ namespace IO.Ably.Tests.Push
             device.FormFactor.Should().NotBeEmpty();
         }
 
-        [Fact(Skip = "Intermittently fails")]
+        [Fact]
         [Trait("spec", "RSH8a")]
         [Trait("spec", "RSH8b")]
         public void LocalDevice_WhenRestClientContainsClientId_ShouldHaveTheSameClientId()
