ce4a0730c334c4fbb0681041c58480bcf1ca2e62
sacOO7 <sachinshinde7676@gmail.com>
2023-09-17 23:57:03 +0530

    Added tests for processing ack/nack when resume success/failure

--- src/IO.Ably.Tests.Shared/Realtime/ConnectionSandboxTransportSideEffectsSpecs.cs
+++ src/IO.Ably.Tests.Shared/Realtime/ConnectionSandboxTransportSideEffectsSpecs.cs
@@ -123,6 +123,134 @@ namespace IO.Ably.Tests.Realtime
             await channel.WaitForState(ChannelState.Detached, TimeSpan.FromSeconds(10));
         }
 
+        [Theory]
+        [ProtocolData]
+        [Trait("spec", "RTN19")]
+        [Trait("spec", "RTN19a")]
+        public async Task OnConnected_ShouldResendAckWithConnectionMessageSerialIfResumeFailed(Protocol protocol)
+        {
+            var client = await GetRealtimeClient(protocol);
+            await client.WaitForState(ConnectionState.Connected);
+            var initialConnectionId = client.Connection.Id;
+            var channelName = "RTN19a".AddRandomSuffix();
+            var channel = client.Channels.Get(channelName);
+
+            // var client2 = await GetRealtimeClient(protocol);
+            // var channel2 = client2.Channels.Get(channelname);
+            // await channel2.AttachAsync();
+            // var channel2Messages = new List<Message>();
+            // channel2.Subscribe(message => channel2Messages.Add(message));
+
+            // Sending dummy messages to increment messageSerial
+            await channel.PublishAsync("dummy1", "data1");
+            await channel.PublishAsync("dummy2", "data2");
+
+            // This will be unblocked on new connection/transport.
+            client.BlockActionFromReceiving(ProtocolMessage.MessageAction.Ack);
+            client.BlockActionFromReceiving(ProtocolMessage.MessageAction.Nack);
+
+            var noOfMessages = 10;
+            var messageAckAwaiter = new TaskCompletionAwaiter(15000, noOfMessages);
+            for (var i = 0; i < noOfMessages; i++)
+            {
+                channel.Publish("eventName" + i, "data" + i, (success, error) =>
+                {
+                    if (success)
+                    {
+                        messageAckAwaiter.Tick();
+                    }
+                });
+            }
+
+            await client.ProcessCommands();
+            client.State.WaitingForAck.Count.Should().Be(noOfMessages);
+            var initialMessagesIdToSerialMap = client.GetTestTransport()
+                .ProtocolMessagesSent.FindAll(message => message.Channel == channel.Name).ToDictionary(m => m.Messages.First().Name, m => m.MsgSerial);
+
+            client.State.Connection.Id = string.Empty;
+            client.State.Connection.Key = "xxxxx!xxxxxxx-xxxxxxxx-xxxxxxxx"; // invalid connection key for next resume request
+            client.GetTestTransport().Close(false);
+
+            await client.WaitForState(ConnectionState.Connected);
+            client.Connection.Id.Should().NotBe(initialConnectionId); // resume not successful
+
+            // Ack received for all messages
+            var messagePublishSuccess = await messageAckAwaiter.Task;
+            messagePublishSuccess.Should().BeTrue();
+
+            var newMessagesIdToSerialMap = client.GetTestTransport()
+                .ProtocolMessagesSent.FindAll(message => message.Channel == channel.Name).ToDictionary(m => m.Messages.First().Name, m => m.MsgSerial);
+
+            // Check for new messageSerial
+            foreach (var keyValuePair in newMessagesIdToSerialMap)
+            {
+                initialMessagesIdToSerialMap[keyValuePair.Key].Should().NotBe(keyValuePair.Value);
+            }
+
+            // TODO - Message duplicates can't be detected since messages id's not available
+            // channel2Messages.Count.Should().Be(12);
+
+            client.Close();
+        }
+
+        [Theory]
+        [ProtocolData]
+        [Trait("spec", "RTN19")]
+        [Trait("spec", "RTN19b")]
+        public async Task OnConnected_ShouldResendAckWithSameMessageSerialIfResumeSuccessful(Protocol protocol)
+        {
+            var client = await GetRealtimeClient(protocol);
+            await client.WaitForState(ConnectionState.Connected);
+            var initialConnectionId = client.State.Connection.Id;
+            var channelName = "RTN19a".AddRandomSuffix();
+            var channel = client.Channels.Get(channelName);
+
+            await channel.PublishAsync("dummy1", "data1");
+            await channel.PublishAsync("dummy2", "data2");
+
+            // This will be unblocked on new connection/transport.
+            client.BlockActionFromReceiving(ProtocolMessage.MessageAction.Ack);
+            client.BlockActionFromReceiving(ProtocolMessage.MessageAction.Nack);
+
+            var noOfMessages = 10;
+            var messageAckAwaiter = new TaskCompletionAwaiter(15000, noOfMessages);
+            for (var i = 0; i < noOfMessages; i++)
+            {
+                channel.Publish("eventName" + i, "data" + i, (success, error) =>
+                {
+                    if (success)
+                    {
+                        messageAckAwaiter.Tick();
+                    }
+                });
+            }
+
+            await client.ProcessCommands();
+            client.State.WaitingForAck.Count.Should().Be(noOfMessages);
+            var initialMessagesIdToSerialMap = client.GetTestTransport()
+                .ProtocolMessagesSent.FindAll(message => message.Channel == channel.Name).ToDictionary(m => m.Messages.First().Name, m => m.MsgSerial);
+
+            client.GetTestTransport().Close(false); // same connectionKey for next request
+
+            await client.WaitForState(ConnectionState.Connected);
+            client.Connection.Id.Should().Be(initialConnectionId); // resume success
+            // Ack received for all messages
+            var messagePublishSuccess = await messageAckAwaiter.Task;
+            messagePublishSuccess.Should().BeTrue();
+
+            var newMessagesIdToSerialMap = client.GetTestTransport()
+                .ProtocolMessagesSent.FindAll(message => message.Channel == channel.Name).ToDictionary(m => m.Messages.First().Name, m => m.MsgSerial);
+
+            // Check for same messageSerial
+            foreach (var keyValuePair in newMessagesIdToSerialMap)
+            {
+                initialMessagesIdToSerialMap[keyValuePair.Key].Should().Be(keyValuePair.Value);
+            }
+
+            client.Close();
+
+        }
+
         public ConnectionSandboxTransportSideEffectsSpecs(AblySandboxFixture fixture, ITestOutputHelper output)
             : base(fixture, output)
         {
